b06e324ef5b34beeb61639c94800800362e7a99c
Łukasz Gandecki <lgandecki@css.edu>
2017-03-09 11:38:16 +0000

    hot module replacement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { AppContainer } from 'react-hot-loader'
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <AppContainer>
+        <App/>
+    </AppContainer>,
+    document.getElementById('root')
 );
+
+// Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp/>
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
