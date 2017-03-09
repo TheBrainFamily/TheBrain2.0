e7b4818e648fb1d05122e1f171adcddf92a0ecc1
Łukasz Gandecki <lgandecki@css.edu>
2017-03-09 11:13:22 +0000

    initial meteor

--- client/main.js
+++ client/main.js
@@ -1,15 +1,1 @@
-import { Template } from 'meteor/templating';  // eslint-disable-line
-import { ReactiveVar } from 'meteor/reactive-var'; // eslint-disable-line
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+import '/imports/client/index';
