31145e292cf9f34c66ec24715d978f3022a9110c
Rachomir <bciach@thebrain.pro>
2017-03-09 16:09:48 +0000

    Working on styles - work in progress

---
+++ client/src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Content extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <h3>Why not to try it for yourself?</h3>
+                <h3>Just follow the instructions visible below</h3>
+                <div>
+                </div>
+            </div>)
+    }
+}
