5438dea515647673712645b7129539151fad59f7
Rachomir <bciach@thebrain.pro>
2017-03-09 18:07:48 +0000

    Changed tutorials autoplay parameter to 0

--- client/src/components/Tutorial.js
+++ client/src/components/Tutorial.js
@@ -19,7 +19,7 @@ class TutorialVideo extends React.Component {
             height: '390',
             width: '640',
             playerVars: { // https://developers.google.com/youtube/player_parameters
-                autoplay: 1
+                autoplay: 0
             }
         };
 
