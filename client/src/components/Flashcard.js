959b24aee6124e6152ec6db61e422da2e9787d75
Rachomir <bciach@thebrain.pro>
2017-03-10 12:03:29 +0000

    Merge

--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export default class extends React.Component {
+
     render() {
         return <div>
-            <div className="flashcard question">{this.props.question}</div>
-            <div className="flashcard answer">{this.props.answer}
+            <div className="center flashcard">QUESTION : <br/><br/><br/>{this.props.question}</div>
+            <div className="center flashcard answer">CORRECT ANSWER :<br/><br/>{this.props.answer}
             </div>
+            <button className="button-answer">Blackout</button>
+            <button className="button-answer">Terrible</button>
+            <button className="button-answer">Bad</button>
+            <button className="button-answer">Hardly</button>
+            <button className="button-answer">Good</button>
+            <button className="button-answer">Perfect!</button>
         </div>
     }
 }
