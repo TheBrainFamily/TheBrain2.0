115f3cd467516b8b7cdfcc8e4d3bfac8cb02b9d9
Łukasz Gandecki <lgandecki@css.edu>
2017-03-11 12:55:23 +0000

    math calculations instead of strings

--- client/src/components/SessionSummary.js
+++ client/src/components/SessionSummary.js
@@ -5,19 +5,19 @@ export default class extends React.Component {
     render() {
         const {newFlashcards, repetitions, extraRepetitions} = this.props;
 
-        const left = `${newFlashcards.todo + newFlashcards.done}`;
-        const done = `${repetitions.todo} / ${repetitions.todo + repetitions.done}`;
-        const toBeRepeated = `${extraRepetitions.todo} / ${extraRepetitions.todo + extraRepetitions.done}`;
+        const left = ((newFlashcards.todo / (newFlashcards.todo + newFlashcards.done)) * 100) || 100;
+        const done = ((repetitions.todo / (repetitions.todo + repetitions.done)) * 100) || 100;
+        const toBeRepeated = ((extraRepetitions.todo / (extraRepetitions.todo + extraRepetitions.done)) * 100) || 100;
 
         return <div>
 
-            <div className="session-summary">Left: {left}</div>
+            <div className="session-summary">Left: {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done}</div>
             <Circle className="progress-bar" percent={left} trailWidth="4" strokeWidth="18" strokeColor="#C8B600" trailColor="#AA9F39" strokeLinecap="square"/>
 
-            <div className="session-summary">Done: {done} </div>
+            <div className="session-summary">Done: {repetitions.todo} / {repetitions.todo + repetitions.done} </div>
             <Circle className="progress-bar" percent={done} trailWidth="4" strokeWidth="18" strokeColor="#FF0000" trailColor="#AA3939" strokeLinecap="square"/>
 
-            <div className="session-summary">To be repeated: {toBeRepeated}</div>
+            <div className="session-summary">To be repeated: {extraRepetitions.todo} / {extraRepetitions.todo + extraRepetitions.done}</div>
             <Circle className="progress-bar" percent={toBeRepeated} trailWidth="4" strokeWidth="18" strokeColor="#0388A7" trailColor="#303E73" strokeLinecap="square"/>
             <br/>
         </div>
