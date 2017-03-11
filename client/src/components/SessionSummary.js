f6f859c555011521aa91405a3d532cd9cc68175a
Rachomir <bciach@thebrain.pro>
2017-03-11 12:31:36 +0000

    Add logic to progress bars

--- client/src/components/SessionSummary.js
+++ client/src/components/SessionSummary.js
@@ -7,12 +7,12 @@ export default class extends React.Component {
         return <div>
 
             <div className="session-summary">Left {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done} </div>
-            <Circle className="progress-bar" percent="40" trailWidth="4" strokeWidth="18" strokeColor="#C8B600" trailColor="#AA9F39" strokeLinecap="square"/>
+            <Circle className="progress-bar" percent={newFlashcards.todo + newFlashcards.done} trailWidth="4" strokeWidth="18" strokeColor="#C8B600" trailColor="#AA9F39" strokeLinecap="square"/>
 
-            <div className="session-summary">Left {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done} </div>
+            <div className="session-summary">Done: {repetitions.todo} / {repetitions.todo + repetitions.done}</div>
             <Circle className="progress-bar" percent="40" trailWidth="4" strokeWidth="18" strokeColor="#FF0000" trailColor="#AA3939" strokeLinecap="square"/>
 
-            <div className="session-summary">Left {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done} </div>
+            <div className="session-summary">To be repeated {extraRepetitions.todo} / {extraRepetitions.todo + extraRepetitions.done}</div>
             <Circle className="progress-bar" percent="40" trailWidth="4" strokeWidth="18" strokeColor="#0388A7" trailColor="#303E73" strokeLinecap="square"/>
             <br/>
         </div>
