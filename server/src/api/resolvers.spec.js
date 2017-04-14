0165f1b32a4876b9860303227f61d15ed257e627
Lukasz Gandecki <lgandecki@css.edu>
2017-04-14 22:31:06 +0200

    added query.CurrentUser and query.Lesson tests

--- server/src/api/resolvers.spec.js
+++ server/src/api/resolvers.spec.js
@@ -5,7 +5,7 @@ import _ from 'lodash'
 import resolvers from './resolvers'
 import {validate} from 'graphql/validation';
 import schema from './schema';
-import {FlashcardsRepository, Flashcards} from './mongooseSetup';
+import {FlashcardsRepository, UserDetailsRepository, LessonsRepository, Flashcards} from './mongooseSetup';
 import {extendExpect, deepFreeze} from 'testHelpers/testHelpers';
 
 extendExpect();
@@ -25,8 +25,9 @@ beforeAll((done) => {
     mongoose.connection.on("open", done);
 });
 
-afterAll(() => {
-    mongoose.disconnect();
+afterAll(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.disconnect();
 });
 
 async function createFlashcard(props) {
@@ -69,12 +70,9 @@ async function makeFlashcards({number = 3, flashcardsToExtend = []} = {}) {
 }
 
 describe('query.flashcards', () => {
-    //
-    // beforeEach(function (done) {
-    //     mongoose.connection.on("open", done);
-    // });
-    afterEach(async() => {
+    afterEach(async(done) => {
         await mongoose.connection.db.dropDatabase();
+        done();
     });
     it('returns flashcards from the db 1', async() => {
         const flashcardsData = await deepFreeze(makeFlashcards());
@@ -89,8 +87,9 @@ describe('query.flashcards', () => {
 });
 
 describe('query.flashcard', () => {
-    afterEach(async() => {
+    afterEach(async(done) => {
         await mongoose.connection.db.dropDatabase();
+        done();
     });
     it('returns a flashcard by id', async() => {
 
@@ -105,6 +104,44 @@ describe('query.flashcard', () => {
         );
 
         expect(dbFlashcards._id).toEqual(flashcardsToExtend[1]._id);
+    })
+});
+
+describe("query.Lesson", () => {
+    beforeAll(async () => {
+        // we have those in different order to make sure the query doesn't return the first inserted lesson.
+        await mongoose.connection.db.collection('lessons').insert({position: 2});
+        await mongoose.connection.db.collection('lessons').insert({position: 1});
+        await mongoose.connection.db.collection('lessons').insert({position: 3});
+    })
+    afterAll(async(done) => {
+        await mongoose.connection.db.dropDatabase();
+        done();
+    })
+    it("returns first lesson for a new user", async () => {
+        const lesson = await resolvers.Query.Lesson(undefined, undefined, {Lessons: new LessonsRepository()});
+
+        expect(lesson).toEqual(expect.objectContaining({position: 1}))
+    })
+    it("returns third lesson for a logged in user that already watched two lessons", async () => {
+        const userId = mongoose.Types.ObjectId();
+        await mongoose.connection.db.collection('userdetails').insert({userId, nextLessonPosition: 3});
+
+        const context = {Lessons: new LessonsRepository(), user: {_id: userId}, UserDetails: new UserDetailsRepository()}
+        const lesson = await resolvers.Query.Lesson(undefined, undefined, context);
+
+        expect(lesson).toEqual(expect.objectContaining({position: 3}))
+    })
+})
+
+describe('query.CurrentUser', () => {
+    it("returns unchanged user from a context", () => {
+        const context = deepFreeze({
+            user: {_id: "testId", email: "test@email.com"}
+        });
+
+        const currentUser = resolvers.Query.CurrentUser(undefined, undefined, context);
+        expect(currentUser).toEqual(context.user)
     })
 })
 
