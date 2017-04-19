0fd9f9a53d5048c6228e0f91d50eeb29787b72d5
Przemysław Gierski <pgierski@o2.pl>
2017-04-19 16:05:19 +0200

    initial test of swipe

--- mobileClient/components/Flashcard.js
+++ mobileClient/components/Flashcard.js
@@ -6,6 +6,7 @@ import {
     Text,
     View,
     Button,
+    StyleSheet,
 } from 'react-native';
 
 class Flashcard extends React.Component {
@@ -15,6 +16,13 @@ class Flashcard extends React.Component {
 
         this.state = {visibleAnswer: false};
     }
+
+    getInitialState = () => {
+        return {
+            x: 0,
+            y: 0
+        }
+    };
 
     answeredQuestion = () => {
         this.setState({visibleAnswer: true})
@@ -28,59 +36,124 @@ class Flashcard extends React.Component {
         this.setState({visibleAnswer: false})
     };
 
+    resetPosition = (e) => {
+        this.dragging = false;
+        //Reset on release
+        this.setState({
+            x: 0,
+            y: 0,
+        })
+    };
+    _onStartShouldSetResponder = (e) => {
+        console.log('PINGWIN: _onStartShouldSetResponder');
+        this.dragging = true;
+        //Setup initial drag coordinates
+        this.drag = {
+            x: e.nativeEvent.pageX,
+            y: e.nativeEvent.pageY
+        };
+        return true;
+    };
+    _onMoveShouldSetResponder = (e) => {
+        return true;
+    };
+
+    setPosition = (event) => {
+        //Update our state with the deltaX/deltaY of the movement
+        this.setState({
+            x: this.state.x + (event.nativeEvent.pageX - this.drag.x),
+            y: this.state.y + (event.nativeEvent.pageY - this.drag.y)
+        });
+        console.log('PINGWIN: this.state', this.state);
+        //Set our drag to be the new position so our delta can be calculated next time correctly
+        this.drag.x = event.nativeEvent.pageX;
+        this.drag.y = event.nativeEvent.pageY;
+    };
+
+    getCardStyle = function () {
+        const transform = [{translateX: this.state.x}, {translateY: this.state.y}];
+        return {transform: transform};
+    };
+
     render() {
-        return <View>
-            <Text>QUESTION : {this.props.question}</Text>
-            <View>{!this.state.visibleAnswer ?
-            <Button
-                onPress={this.answeredQuestion}
-                title="SHOW ANSWER"
-                color="#841584"
-                accessibilityLabel="SHOW ANSWER"
-            />
-                 :
-                <View>
-                    <Text>CORRECT ANSWER :{this.props.answer}
-                    </Text>
-                    <Text>How would you describe experience answering this question?</Text>
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(1)}
-                        title="Blackout"
-                        color="#841584"
-                        accessibilityLabel="Blackout"
-                    />
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(2)}
-                        title="Terrible"
-                        color="#841584"
-                        accessibilityLabel="Terrible"
-                    />
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(3)}
-                        title="Bad"
-                        color="#841584"
-                        accessibilityLabel="Bad"
-                    />
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(4)}
-                        title="Hardly"
-                        color="#841584"
-                        accessibilityLabel="Hardly"
-                    />
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(5)}
-                        title="Good"
-                        color="#841584"
-                        accessibilityLabel="Good"
-                    />
-                    <Button
-                        onPress={() => this.onSubmitEvaluation(6)}
-                        title="Perfect!"
-                        color="#841584"
-                        accessibilityLabel="Perfect!"
-                    />
-                </View>
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            },
+            card: {
+                borderWidth: 3,
+                borderRadius: 3,
+                borderColor: '#000',
+                width: 300,
+                height: 300,
+                padding: 10,
+                marginTop: 300,
             }
+        });
+
+        // const style = this.getCardStyle();
+        // console.log('PINGWIN: style', style);
+        console.log('PINGWIN: dziala');
+        return <View style={styles.container}>
+            <View style={[styles.card]}
+                  onResponderMove={this.setPosition}
+                  onResponderRelease={this.resetPosition}
+                  onStartShouldSetResponder={this._onStartShouldSetResponder}
+                  onMoveShouldSetResponder={this._onMoveShouldSetResponder}>
+                <Text>QUESTION : {this.props.question}</Text>
+                <View>{!this.state.visibleAnswer ?
+                    <Button
+                        onPress={this.answeredQuestion}
+                        title="SHOW ANSWER"
+                        color="#841584"
+                        accessibilityLabel="SHOW ANSWER"
+                    />
+                    :
+                    <View>
+                        <Text>CORRECT ANSWER :{this.props.answer}
+                        </Text>
+                        <Text>How would you describe experience answering this question?</Text>
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(1)}
+                            title="Blackout"
+                            color="#841584"
+                            accessibilityLabel="Blackout"
+                        />
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(2)}
+                            title="Terrible"
+                            color="#841584"
+                            accessibilityLabel="Terrible"
+                        />
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(3)}
+                            title="Bad"
+                            color="#841584"
+                            accessibilityLabel="Bad"
+                        />
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(4)}
+                            title="Hardly"
+                            color="#841584"
+                            accessibilityLabel="Hardly"
+                        />
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(5)}
+                            title="Good"
+                            color="#841584"
+                            accessibilityLabel="Good"
+                        />
+                        <Button
+                            onPress={() => this.onSubmitEvaluation(6)}
+                            title="Perfect!"
+                            color="#841584"
+                            accessibilityLabel="Perfect!"
+                        />
+                    </View>
+                }
+                </View>
             </View>
         </View>
     }
