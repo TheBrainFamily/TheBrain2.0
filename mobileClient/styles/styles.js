76ff674cdf714fc62ff68270e8ec7c65848cb74c
lolezy <mopidowicz@thebrain.pro>
2017-04-19 16:22:43 +0200

    added default styles, styling of summary container

--- mobileClient/styles/styles.js
+++ mobileClient/styles/styles.js
@@ -16,5 +16,63 @@ export default styles = StyleSheet.create({
         backgroundColor: '#E5BA9E',
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    summaryContainer: {
+        flexDirection: 'row',
+        padding: 5,
+        backgroundColor: 'black',
+    },
+    summaryLeftRow: {
+        color: 'white',
+        fontSize: 10,
+        textAlign: 'left',
+        width: 150,
+    },
+    summaryCenterRow: {
+        color: 'white',
+        fontSize: 10,
+        textAlign: 'center',
+        flex: 1,
+    },
+    summaryRightRow: {
+        color: 'white',
+        fontSize: 10,
+        textAlign: 'right',
+        width: 150,
+    },
+    primaryHeader: {
+        color: 'white',
+        backgroundColor: 'steelblue',
+        fontSize: 10,
+        padding: 5,
+        textAlign: 'center',
+    },
+    primaryText: {
+        color: 'black',
+        backgroundColor: '#DDD',
+        fontSize: 20,
+        padding: 10,
+        textAlign: 'center',
+    },
+    secondaryHeader: {
+        color: 'white',
+        backgroundColor: 'steelblue',
+        fontSize: 10,
+        padding: 5,
+        textAlign: 'center',
+    },
+    secondaryText: {
+        color: 'black',
+        backgroundColor: '#DDD',
+        fontSize: 20,
+        padding: 10,
+        textAlign: 'center',
+    },
+    tertiaryHeader: {
+        color: 'black',
+        backgroundColor: 'skyblue',
+        fontSize: 10,
+        padding: 5,
+        textAlign: 'center',
+    },
 });
