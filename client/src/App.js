effb3fa058f7bc112a997abea77166eb7294dd3c
Andżelo <andre2go@gmail.com>
2017-04-20 18:02:51 +0200

    Integrate Apollo client with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-import { Provider } from 'react-redux';
 import { Route } from 'react-router';
 import { ConnectedRouter as Router } from 'react-router-redux';
-import {ApolloClient, createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 
-import store, { history } from './store'
+import store, { history, client } from './store'
 import './App.css';
 import Home from './components/Home';
 import WellDone from './components/WellDone';
@@ -17,55 +15,42 @@ import Signup from './components/Signup';
 import Header from './components/Header';
 import ResetPassword from './components/ResetPassword';
 
-let uri;
-const localhostRegexp = /localhost/;
-if (localhostRegexp.test(window.location.origin)) {
-    uri = 'http://localhost:8080/graphql';
-} else {
-    uri = 'http://new.thebrain.pro:8080/graphql';
-}
-console.log("Gozdecki: uri",uri);
-
-const networkInterface = createNetworkInterface({
-    uri,
-    opts: {
-        credentials: 'include',
-    },
-});
-
-const client = new ApolloClient({
-    networkInterface,
-});
-
 class App extends Component {
+    getChildContext() {
+        return {
+            store
+        }
+    }
     render() {
         return (
-            <Provider store={store}>
+            <ApolloProvider client={client} store={store}>
                 <Router history={history}>
-                    <ApolloProvider client={client}>
-                        <div className="App">
-                            <Header/>
-                            <div className="App-intro styleIntroduction">
-                                <Route exact key="tutorial" path="/" component={Home}/>
-                                <Route exact key="Lecture" path="/lecture"
-                                       component={Lecture}/>
+                    <div className="App">
+                        <Header/>
+                        <div className="App-intro styleIntroduction">
+                            <Route exact key="tutorial" path="/" component={Home}/>
+                            <Route exact key="Lecture" path="/lecture"
+                                   component={Lecture}/>
 
-                                <Route exact key="wellDone" path="/wellDone"
-                                       component={WellDone}/>
+                            <Route exact key="wellDone" path="/wellDone"
+                                   component={WellDone}/>
 
-                                <Route exact key="questions" path="/questions"
-                                       component={Questions}/>
-                                <Route exact key="login" path="/login" component={Login}/>
-                                <Route exact key="signup" path="/signup" component={Signup}/>
-                                <Route exact key="resetpassword" path="/resetpassword" component={ResetPassword}/>
-                                <Footer/>
-                            </div>
+                            <Route exact key="questions" path="/questions"
+                                   component={Questions}/>
+                            <Route exact key="login" path="/login" component={Login}/>
+                            <Route exact key="signup" path="/signup" component={Signup}/>
+                            <Route exact key="resetpassword" path="/resetpassword" component={ResetPassword}/>
+                            <Footer/>
                         </div>
-                    </ApolloProvider>
+                    </div>
                 </Router>
-            </Provider>
+            </ApolloProvider>
         );
     }
+}
+
+App.childContextTypes = {
+    store: React.PropTypes.object,
 }
 
 export default App;
--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,47 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux'
 import createHistory from 'history/createBrowserHistory'
 import { routerMiddleware, routerReducer } from 'react-router-redux'
+import {ApolloClient, createNetworkInterface} from 'apollo-client';
+
+let uri;
+const localhostRegexp = /localhost/;
+if (localhostRegexp.test(window.location.origin)) {
+  uri = 'http://localhost:8080/graphql';
+} else {
+  uri = 'http://new.thebrain.pro:8080/graphql';
+}
+console.log("Gozdecki: uri",uri);
+
+const networkInterface = createNetworkInterface({
+  uri,
+  opts: {
+    credentials: 'include',
+  },
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
 // import reducers from './reducers'
 
 const history = createHistory()
-const middleware = routerMiddleware(history)
+
+const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
 
 const store = createStore(
   combineReducers({
     // ...reducers,
-    router: routerReducer
+    router: routerReducer,
+    apollo: client.reducer(),
   }),
-  applyMiddleware(middleware)
+  {}, // initial state
+  compose(
+    applyMiddleware(client.middleware(), routerMiddleware(history)),
+    devToolsExtension ? devToolsExtension() : f => f,
+  )
 )
 
 export { history }
+export { client }
 export default store
