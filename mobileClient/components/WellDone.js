9734d3128a2cdb2acf2a89cf5e91ef90b832c5df
lolezy <mopidowicz@thebrain.pro>
2017-04-21 18:04:15 +0200

    styling of first page

--- mobileClient/components/WellDone.js
+++ mobileClient/components/WellDone.js
@@ -16,18 +16,13 @@ export class WellDone extends React.Component {
     }
 
     render() {
-        return <View style={styles.contentBox}>
-            <View>
-                <Text style={styles.secondaryText}>First video done!</Text>
-            </View>
-            <View>
-                <Link to="/Questions">
-                    <Text style={styles.primaryText}>Tap here to answer some questions about the video</Text>
-                </Link>
-            </View>
-            <View>
-                <Text style={styles.secondaryText}> </Text>
-            </View>
+        return <View style={styles.wellDonePage}>
+            <Link to="/Questions">
+                <View style={styles.wellDoneContainer}>
+                    <Text style={[styles.primaryHeader, styles.wellDoneHeader]}>FIRST VIDEO DONE!</Text>
+                    <Text style={[styles.primaryHeader, styles.wellDoneContent]}>Tap anywhere to answer some questions about the video</Text>
+                </View>
+            </Link>
         </View>
     }
 }
