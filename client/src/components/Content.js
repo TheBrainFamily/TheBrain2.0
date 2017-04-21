c77ff39ec84685006a358a2685efc65ec5afbc89
Andżelo <andre2go@gmail.com>
2017-04-21 15:36:19 +0200

    Reformat code in client/src

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 export default class Content extends React.Component {
-    render() {
-        return (
-            <div className="content">
-                <h2>Why don't you try it yourself?</h2>
-                <h2>Just whatch a video below and start your fascinating journey with TheBrain !</h2>
-            </div>)
-    }
+  render() {
+    return (
+      <div className="content">
+        <h2>Why don't you try it yourself?</h2>
+        <h2>Just whatch a video below and start your fascinating journey with TheBrain !</h2>
+      </div>)
+  }
 }
--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -22,27 +22,27 @@ class Flashcard extends React.Component {
 
   render() {
     return <div>
-        <div className="center flashcard">QUESTION : <br /><br /><br />{this.props.question}</div>
+      <div className="center flashcard">QUESTION : <br /><br /><br />{this.props.question}</div>
 
-        <br />
-        <br />
+      <br />
+      <br />
 
-        <div>{!this.props.isAnswerVisible ?
-          <button className="button-answer" onClick={this.answeredQuestion}>SHOW ANSWER</button> :
-          <div>
-              <div className="center flashcard answer">CORRECT ANSWER :<br /><br />{this.props.answer}
-              </div>
-              <p>How would you describe experience answering this question?</p>
-              <br />
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(1)}>Blackout</button>
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(2)}>Terrible</button>
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(3)}>Bad</button>
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(4)}>Hardly</button>
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(5)}>Good</button>
-              <button className="button-answer" onClick={() => this.onSubmitEvaluation(6)}>Perfect!</button>
+      <div>{!this.props.isAnswerVisible ?
+        <button className="button-answer" onClick={this.answeredQuestion}>SHOW ANSWER</button> :
+        <div>
+          <div className="center flashcard answer">CORRECT ANSWER :<br /><br />{this.props.answer}
           </div>
-        }
+          <p>How would you describe experience answering this question?</p>
+          <br />
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(1)}>Blackout</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(2)}>Terrible</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(3)}>Bad</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(4)}>Hardly</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(5)}>Good</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(6)}>Perfect!</button>
         </div>
+      }
+      </div>
     </div>
   }
 }
