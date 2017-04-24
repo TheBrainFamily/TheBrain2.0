513775f40c73e260e72fc92aeae3009de1ce2d76
pwojcik <pwojcik@thebrain.pro>
2017-04-24 17:46:12 +0200

    Refactored emoji related components and removed position state from redux

--- mobileClient/reducers/FlashcardReducer.js
+++ mobileClient/reducers/FlashcardReducer.js
@@ -2,8 +2,6 @@ const reducer = (state = { x: 0, y: 0 }, action) => {
   switch (action.type) {
     case 'UPDATE_ANSWER_VISIBILITY':
       return { ...state, visibleAnswer: action.visibleAnswer };
-    case 'UPDATE_POSITION':
-      return { ...state, x: action.x, y: action.y };
     default:
       return state;
   }
