d35d05c578e772ae4e2719d682e5ec3fadb6b932
Przemysław Gierski <pgierski@o2.pl>
2017-04-25 14:42:10 +0200

    create blank signup component

---
+++ mobileClient/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+class Signup extends React.Component {
+
+    render = () => {
+        return (<View><Text>In Signup</Text></View>);
+    };
+
+}
