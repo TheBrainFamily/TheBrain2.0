353e51d7a87459b0c28ea0c879d785f279ba818d
Tomasz Wilczek <twilczek@thebrain.pro>
2017-04-25 17:08:43 +0200

    Remove warning ‚Cannot update during an existing state transition’ appearing when updating Questions and Home components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,9 +6,20 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 
 class Home extends React.Component {
-  render () {
-    const {dispatch} = this.props
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.data || nextProps.data.loading || nextProps.data.error) {
+      return
+    }
 
+    if (nextProps.data.ItemsWithFlashcard.length > 0) {
+      nextProps.dispatch(push("/questions"));
+
+    } else {
+      nextProps.dispatch(push("/lecture"))
+
+    }
+  }
+  render () {
     if (this.props.data.loading) {
       return (<p>Loading...</p>)
     }
@@ -16,11 +27,7 @@ class Home extends React.Component {
     if (this.props.data.error) {
       return (<p>Error...</p>)
     }
-    if (this.props.data.ItemsWithFlashcard.length > 0) {
-      dispatch(push('/questions'))
-    } else {
-      dispatch(push('/lecture'))
-    }
+
     return <div></div>
   }
 }
