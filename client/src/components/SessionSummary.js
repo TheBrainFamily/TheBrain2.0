567eda3a0cd83aa9447c67b212a6744d04f74e89
Andżelo <andre2go@gmail.com>
2017-04-26 15:20:04 +0200

    Format all files in client/src according to JavaScript Standard Style

--- client/src/components/SessionSummary.js
+++ client/src/components/SessionSummary.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import { Circle } from 'rc-progress';
+import React from 'react'
+import { Circle } from 'rc-progress'
 
 export default class extends React.Component {
-    render() {
-        const {newFlashcards, repetitions, extraRepetitions} = this.props;
+  render() {
+    const { newFlashcards, repetitions, extraRepetitions } = this.props
 
-        const left = ((newFlashcards.todo / (newFlashcards.todo + newFlashcards.done)) * 100) || 100;
-        const done = ((repetitions.todo / (repetitions.todo + repetitions.done)) * 100) || 100;
-        const toBeRepeated = ((extraRepetitions.todo / (extraRepetitions.todo + extraRepetitions.done)) * 100) || 100;
+    const left = ((newFlashcards.todo / (newFlashcards.todo + newFlashcards.done)) * 100) || 100
+    const done = ((repetitions.todo / (repetitions.todo + repetitions.done)) * 100) || 100
+    const toBeRepeated = ((extraRepetitions.todo / (extraRepetitions.todo + extraRepetitions.done)) * 100) || 100
 
-        return <div>
+    return <div>
 
-            <div className="session-summary">Left: {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done}</div>
-            <Circle className="progress-bar" percent={left} trailWidth="4" strokeWidth="18" strokeColor="#C8B600" trailColor="#AA9F39" strokeLinecap="square"/>
+      <div className="session-summary">Left: {newFlashcards.todo} / {newFlashcards.todo + newFlashcards.done}</div>
+      <Circle className="progress-bar"
+              percent={left}
+              trailWidth="4"
+              strokeWidth="18"
+              strokeColor="#C8B600"
+              trailColor="#AA9F39"
+              strokeLinecap="square" />
 
-            <div className="session-summary">Done: {repetitions.todo} / {repetitions.todo + repetitions.done} </div>
-            <Circle className="progress-bar" percent={done} trailWidth="4" strokeWidth="18" strokeColor="#FF0000" trailColor="#AA3939" strokeLinecap="square"/>
+      <div className="session-summary">Done: {repetitions.todo} / {repetitions.todo + repetitions.done} </div>
+      <Circle className="progress-bar"
+              percent={done}
+              trailWidth="4"
+              strokeWidth="18"
+              strokeColor="#FF0000"
+              trailColor="#AA3939"
+              strokeLinecap="square" />
 
-            <div className="session-summary">To be repeated: {extraRepetitions.todo} / {extraRepetitions.todo + extraRepetitions.done}</div>
-            <Circle className="progress-bar" percent={toBeRepeated} trailWidth="4" strokeWidth="18" strokeColor="#0388A7" trailColor="#303E73" strokeLinecap="square"/>
-            <br/>
-        </div>
-    }
+      <div className="session-summary">To be repeated: {extraRepetitions.todo}
+        / {extraRepetitions.todo + extraRepetitions.done}</div>
+      <Circle className="progress-bar"
+              percent={toBeRepeated}
+              trailWidth="4"
+              strokeWidth="18"
+              strokeColor="#0388A7"
+              trailColor="#303E73"
+              strokeLinecap="square" />
+      <br />
+    </div>
+  }
 }
