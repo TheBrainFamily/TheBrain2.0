af836f08fc4050f46e7652127a8208fa6c3e38d0
Andżelo <andre2go@gmail.com>
2017-04-26 15:20:04 +0200

    Format all files in client/src according to JavaScript Standard Style

--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import {graphql} from 'react-apollo';
-import gql from 'graphql-tag';
-import update from 'immutability-helper';
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import update from 'immutability-helper'
 
 class Flashcard extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.state = {visibleAnswer: false};
-    }
+    this.state = { visibleAnswer: false }
+  }
 
-    answeredQuestion = () => {
-        this.setState({visibleAnswer: true})
-    };
+  answeredQuestion = () => {
+    this.setState({ visibleAnswer: true })
+  }
 
-    onSubmitEvaluation = (value) => {
-        this.props.submit({
-            itemId: this.props.evalItemId,
-            evaluation: value
-        });
-        this.setState({visibleAnswer: false})
-    };
+  onSubmitEvaluation = (value) => {
+    this.props.submit({
+      itemId: this.props.evalItemId,
+      evaluation: value
+    })
+    this.setState({ visibleAnswer: false })
+  }
 
-    render() {
-        return <div>
-            <div className="center flashcard">QUESTION : <br/><br/><br/>{this.props.question}</div>
+  render() {
+    return <div>
+      <div className="center flashcard">QUESTION : <br /><br /><br />{this.props.question}</div>
 
-            <br/>
-            <br/>
+      <br />
+      <br />
 
-            <div>{!this.state.visibleAnswer ?
-                <button className="button-answer" onClick={this.answeredQuestion}>SHOW ANSWER</button> :
-                    <div>
-                        <div className="center flashcard answer">CORRECT ANSWER :<br/><br/>{this.props.answer}
-                    </div>
-                    <p>How would you describe experience answering this question?</p>
-                    <br/>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(1)} >Blackout</button>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(2)} >Terrible</button>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(3)} >Bad</button>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(4)} >Hardly</button>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(5)} >Good</button>
-                    <button className="button-answer" onClick={()=>this.onSubmitEvaluation(6)} >Perfect!</button>
-                </div>
-            }
-            </div>
+      <div>{!this.state.visibleAnswer ?
+        <button className="button-answer" onClick={this.answeredQuestion}>SHOW ANSWER</button> :
+        <div>
+          <div className="center flashcard answer">CORRECT ANSWER :<br /><br />{this.props.answer}
+          </div>
+          <p>How would you describe experience answering this question?</p>
+          <br />
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(1)}>Blackout</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(2)}>Terrible</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(3)}>Bad</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(4)}>Hardly</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(5)}>Good</button>
+          <button className="button-answer" onClick={() => this.onSubmitEvaluation(6)}>Perfect!</button>
         </div>
-    }
+      }
+      </div>
+    </div>
+  }
 }
 
 const submitEval = gql`    
@@ -65,26 +65,26 @@ const submitEval = gql`
             }
         }
     }
-`;
+`
 
 export default graphql(submitEval, {
-    props: ({ownProps, mutate}) => ({
-        submit: ({itemId, evaluation}) => mutate({
-            variables: {
-                itemId,
-                evaluation,
-            },
-            updateQueries: {
-                CurrentItems: (prev, {mutationResult}) => {
-                    const updateResults = update(prev, {
-                        ItemsWithFlashcard: {
-                            $set: mutationResult.data.processEvaluation
-                        }
-                    });
-                    return updateResults;
-                }
+  props: ({ ownProps, mutate }) => ({
+    submit: ({ itemId, evaluation }) => mutate({
+      variables: {
+        itemId,
+        evaluation,
+      },
+      updateQueries: {
+        CurrentItems: (prev, { mutationResult }) => {
+          const updateResults = update(prev, {
+            ItemsWithFlashcard: {
+              $set: mutationResult.data.processEvaluation
             }
-        })
+          })
+          return updateResults
+        }
+      }
     })
-})(Flashcard);
+  })
+})(Flashcard)
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import {graphql} from 'react-apollo';
-import gql from 'graphql-tag';
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import { withRouter } from 'react-router'
 
 class Signup extends React.Component {
-    submit = (e) => {
-        e.preventDefault();
-      this.props.submit({username: this.refs.username.value, password: this.refs.password.value})
-          .then(() => {
-              this.props.history.push("/");
-          });
-    };
-    render() {
-        return (
-            <form onSubmit={this.submit}>
-                <div>
-                    <label>Username:</label>
-                    <input ref="username" type="text" name="username"/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input ref="password" type="password" name="password"/>
-                </div>
-                <div>
-                    <input type="submit" value="Signup"/>
-                </div>
-            </form>)
-    };
+  submit = (e) => {
+    e.preventDefault()
+    this.props.submit({ username: this.refs.username.value, password: this.refs.password.value })
+      .then(() => {
+        this.props.history.push('/')
+      })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submit}>
+        <div>
+          <label>Username:</label>
+          <input ref="username" type="text" name="username" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input ref="password" type="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" value="Signup" />
+        </div>
+      </form>)
+  };
 }
 
 const signup = gql`
@@ -35,15 +36,15 @@ const signup = gql`
             username
         }
     }
-`;
+`
 
 export default withRouter(graphql(signup, {
-    props: ({ownProps, mutate}) => ({
-        submit: ({username, password}) => mutate({
-            variables: {
-                username,
-                password
-            }
-        })
+  props: ({ ownProps, mutate }) => ({
+    submit: ({ username, password }) => mutate({
+      variables: {
+        username,
+        password
+      }
     })
-})(Signup));
+  })
+})(Signup))
