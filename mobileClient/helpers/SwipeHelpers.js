86769bdab5cf667fd95179dfa9408c43b738b796
lolezy <mopidowicz@thebrain.pro>
2017-05-10 15:05:51 +0200

    refactored direction type, variable names and functions

--- mobileClient/helpers/SwipeHelpers.js
+++ mobileClient/helpers/SwipeHelpers.js
@@ -1,34 +1,34 @@
 // @flow
+import _ from 'lodash'
 
-const DIRECTION_MAP = {
+export const DIRECTIONS = {
+    'left': 'left',
+    'top': 'top',
+    'right': 'right',
+    'bottom': 'bottom'
+}
+
+export type Direction = $Keys<typeof DIRECTIONS>
+
+const directionKeys = {
   '1': 'left',
   '2': 'top',
   '3': 'right',
   '4': 'bottom'
 }
 
-export const DIRECTION_VAL = {
-  'left': 1,
-  'top': 2,
-  'right': 3,
-  'bottom': 4
-}
+const directionEvaluationValues = _.invert(directionKeys);
 
-export const DIRECTION_PROTO = {
-  'left': 'left',
-  'top': 'top',
-  'right': 'right',
-  'bottom': 'bottom'
-}
-
-export type DIRECTION = $Keys<typeof DIRECTION_PROTO>
-
-export function calculateSwipeDirection(x: number, y: number) {
+export function calculateSwipeDirection (x: number, y: number): Direction {
   const angleDeg = Math.atan2(y - 0, x - 0) * 180 / Math.PI;
   const directionValue = (Math.round(angleDeg / 90) + 2) % 4 + 1;
-  return DIRECTION_MAP[directionValue];
+  return directionKeys[directionValue];
 }
 
 export function calculateDragLength (x: number, y: number) {
   return Math.sqrt((Math.pow(y, 2) + Math.pow(x, 2)));
+}
+
+export function directionEvaluationValue (direction: Direction): number {
+  return directionEvaluationValues[direction];
 }
