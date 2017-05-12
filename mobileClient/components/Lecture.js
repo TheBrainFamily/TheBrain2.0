3a5c44db523306e6f7100a1a065151e826853e3d
Przemysław Gierski <pgierski@o2.pl>
2017-05-12 16:41:05 +0200

    move updating of lesson number to Lecture at the end of the movie

--- mobileClient/components/Lecture.js
+++ mobileClient/components/Lecture.js
@@ -1,71 +1,75 @@
 // @flow
 
-import React from 'react';
-import YouTube from 'react-native-youtube';
-import { Text, View, StyleSheet } from 'react-native';
-// import Introduction from './Introduction';
-// import Content from './Content';
-import {graphql} from 'react-apollo';
-import gql from 'graphql-tag';
-import { withRouter, Link } from 'react-router-native'
+import React from 'react'
+import YouTube from 'react-native-youtube'
+import { Text } from 'react-native'
+import { compose, graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import { withRouter } from 'react-router-native'
 
+import lessonWatchedMutationParams from '../../client/shared/graphql/mutations/lessonWatchedMutationParams'
+import lessonWatchedMutationSchema from '../../client/shared/graphql/queries/lessonWatchedMutationSchema'
 
 class Lecture extends React.Component {
+  render () {
 
-
-    render() {
-
-        if (this.props.data.loading) {
-            return (<Text>Loading...</Text>)
-        }
-
-        if (this.props.data.error) {
-            return (<Text>Error... Check if server is running.</Text>)
-        }
-
-        return <LectureVideoWithRouter lesson={this.props.data.Lesson}/>
+    if (this.props.data.loading) {
+      return (<Text>Loading...</Text>)
     }
+
+    if (this.props.data.error) {
+      return (<Text>Error... Check if server is running.</Text>)
+    }
+
+    return <LectureVideoWithRouter lesson={this.props.data.Lesson}/>
+  }
 }
 
 class LectureVideo extends React.Component {
-    state: Object;
-    constructor(props: Object) {
-      super(props);
-      this.state = {
-        isReady: false,
-      };
+  state: Object
+
+  constructor (props: Object) {
+    super(props)
+    this.state = {
+      isReady: false,
     }
-    render() {
-        return (
-            <YouTube
-                ref="youtubePlayer"
-                videoId={this.props.lesson.youtubeId} // The YouTube video ID
-                play={true}           // control playback of video with true/false
-                hidden={false}        // control visiblity of the entire view
-                playsInline={false}    // control whether the video should play inline
-                loop={false}          // control whether the video should loop when ended
-                showinfo={false}
-                modestbranding={false}
-                rel={false}
+  }
+
+  render () {
+    return (
+      <YouTube
+        ref="youtubePlayer"
+        videoId={this.props.lesson.youtubeId} // The YouTube video ID
+        play={true}           // control playback of video with true/false
+        hidden={false}        // control visiblity of the entire view
+        playsInline={false}    // control whether the video should play inline
+        loop={false}          // control whether the video should loop when ended
+        showinfo={false}
+        modestbranding={false}
+        rel={false}
 
 
-                onChangeState={this._onChangeState}
+        onChangeState={this._onChangeState}
 
-                style={{alignSelf: 'stretch', height: 300, backgroundColor: 'red', marginVertical: 10}}
-            />
+        style={{alignSelf: 'stretch', height: 300, backgroundColor: 'red', marginVertical: 10}}
+      />
 
-        );
+    )
+  }
+
+  _onChangeState = (event) => {
+    console.log('Gozdecki: event', event)
+    if (event.state === 'ended') {
+      this.props.lessonWatchedMutation()
+      this.props.history.push('/wellDone')
     }
-
-    _onChangeState = (event) => {
-        console.log("Gozdecki: event",event);
-        if (event.state === "ended") {
-            this.props.history.push("/wellDone");
-        }
-    }
+  }
 }
 
-const LectureVideoWithRouter = withRouter(LectureVideo);
+const LectureVideoWithRouter = compose(
+  graphql(lessonWatchedMutationSchema, lessonWatchedMutationParams),
+  withRouter
+)(LectureVideo)
 
 const query = gql`
     query Lesson {
@@ -73,8 +77,7 @@ const query = gql`
             _id, position, description, flashcardIds, youtubeId
         }
     }
-`;
+`
 
-
-export default graphql(query)(Lecture);
+export default graphql(query)(Lecture)
 
--- mobileClient/components/WellDone.js
+++ mobileClient/components/WellDone.js
@@ -1,56 +1,25 @@
 // @flow
 
-import React from 'react';
-import {graphql} from 'react-apollo';
-import gql from 'graphql-tag';
-import update from 'immutability-helper';
-import {Link} from 'react-router-native';
+import React from 'react'
+import { Link } from 'react-router-native'
 import {
-    Text,
-    View,
-    Button
-} from 'react-native';
-import styles from '../styles/styles';
+  Text,
+  View,
+} from 'react-native'
+import styles from '../styles/styles'
 
 export class WellDone extends React.Component {
-    componentDidMount() {
-        this.props.lessonWatchedMutation();
-    }
-
-    render() {
-        return <View style={styles.wellDonePage}>
-            <Link to="/Questions">
-                <View style={styles.wellDoneContainer}>
-                    <Text style={[styles.primaryHeader, styles.wellDoneHeader]}>FIRST VIDEO DONE!</Text>
-                    <Text style={[styles.primaryHeader, styles.wellDoneContent]}>Tap anywhere to answer some questions about the video</Text>
-                </View>
-            </Link>
+  render () {
+    return <View style={styles.wellDonePage}>
+      <Link to="/Questions">
+        <View style={styles.wellDoneContainer}>
+          <Text style={[styles.primaryHeader, styles.wellDoneHeader]}>FIRST VIDEO DONE!</Text>
+          <Text style={[styles.primaryHeader, styles.wellDoneContent]}>Tap anywhere to answer some questions about the
+            video</Text>
         </View>
-    }
+      </Link>
+    </View>
+  }
 }
 
-const lessonWatchedMutationSchema = gql`
-    mutation createItemsAndMarkLessonAsWatched{
-        createItemsAndMarkLessonAsWatched{
-            _id, position, description, flashcardIds, youtubeId
-        }
-    }
-`;
-
-
-export default graphql(lessonWatchedMutationSchema, {
-    props: ({ownProps, mutate}) => ({
-        lessonWatchedMutation: () => mutate({
-            updateQueries: {
-                Lesson: (prev, {mutationResult}) => {
-                    const updateResults = update(prev, {
-                        Lesson: {
-                            $set: mutationResult.data.createItemsAndMarkLessonAsWatched
-                        }
-                    });
-                    return updateResults;
-                }
-            }
-        })
-    })
-})(WellDone);
+export default WellDone
