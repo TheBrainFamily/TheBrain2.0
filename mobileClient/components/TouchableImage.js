b9a904eb345ebf91e2446660ec3a55fa98d6fe06
lolezy <mopidowicz@thebrain.pro>
2017-05-12 18:08:21 +0200

    added clickable image on flashcard - image resizes onPress

--- mobileClient/components/TouchableImage.js
+++ mobileClient/components/TouchableImage.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import {
   Image,
-  Text,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import styles from '../styles/styles';
@@ -11,30 +11,48 @@ import styles from '../styles/styles';
 export default class TouchableImage extends React.Component {
   imageProperties: {
     source: string,
-    style: Object
+    style: Object,
+    display: boolean
   };
 
   constructor(props: Object) {
     super(props);
     this.imageProperties = {
       source: 'http://team.thebrain.pro/img/logo.png',
-      style: { width: '50%', height: '50%' }
+      style: {}
     }
+    this.state = {
+      imageSizeStyle:
+        {
+          width: '20%',
+          height: '20%'
+        },
+      enlarged: false
+    };
+
   }
 
-  changeImageSize = () => {
-    if (this.props.clickable) {
-      console.log('image pressed', this.props);
+  componentWillReceiveProps() {
+    this.setState({ imageSizeStyle: { width: '20%', height: '20%' }, enlarged: false });
+  }
+
+  changeImageSize = (event) => {
+    if (!this.state.enlarged) {
+      this.setState({ imageSizeStyle: { width: '50%', height: '50%' }, enlarged: true });
+    } else {
+      this.setState({ imageSizeStyle: { width: '20%', height: '20%' }, enlarged: false });
     }
   }
 
   render = () => {
     return (
-      <Image
-        onPress={this.changeImageSize()}
-        style={this.imageProperties.style}
-        source={{uri: this.imageProperties.source}}
-      />
+      <TouchableWithoutFeedback onPress={this.changeImageSize}>
+          <Image
+          style={[this.imageProperties.style, this.state.imageSizeStyle]}
+          source={{uri: this.imageProperties.source}}
+          resizeMode="contain"
+        />
+      </TouchableWithoutFeedback>
     )
   }
 }
