f684109de8ca0d65d4b7a39513aea77310bc0576
lolezy <mopidowicz@thebrain.pro>
2017-05-16 18:12:05 +0200

    refactoring and linting

--- mobileClient/components/TouchableImage.js
+++ mobileClient/components/TouchableImage.js
@@ -6,99 +6,37 @@ import {
   Image,
   TouchableWithoutFeedback,
   View,
-  Dimensions,
-  StyleSheet
 } from 'react-native';
 import Animator from './Animator'
-import styles from '../styles/styles';
 
 export default class TouchableImage extends React.Component {
-  imageProperties: {
-    source: string,
-    style: Object,
-    display: boolean
-  };
-
   constructor(props: Object) {
-    super(props);
-    this.imageProperties = {
-      source: 'http://team.thebrain.pro/img/logo.png',
-      style: {
-        width: 100,
-        height: 100,
-      }
-    }
+    super(props)
 
     this.layoutStyle = {
       position: 'absolute',
       zIndex: 9999,
     }
 
-    const { width, height } = Dimensions.get('window');
-
     this.state = {
-      animator: new Animator({
-        width: {
-          initial: this.imageProperties.style.width,
-          final: width,
-        },
-        height: {
-          initial: this.imageProperties.style.height,
-          final: this.getFlashcardContentHeight(height),
-        },
-        right: {
-          initial: 30,
-          final: 0
-        },
-        top: {
-          initial: 30,
-          final: 0
-        },
-        shadowRadius: {
-          initial: 4,
-          final: 10
-        },
-        shadowOpacity: {
-          initial: 0.5,
-          final: 1
-        }
-      })
-    };
+      animator: new Animator(this.props.animator)
+    }
   }
 
   componentWillReceiveProps() {
-    this.state.animator.resetAnimations();
-  }
-
-  //should be exported/imported, duplicate code
-  getFlashcardContentHeight = (height) => {
-    const heightOfOtherElements =
-      StyleSheet.flatten(styles.topContainer).height +
-      StyleSheet.flatten(styles.summaryContainer).height +
-      2 * StyleSheet.flatten(styles.primaryHeader).height;
-    return height - heightOfOtherElements;
-  };
-
-  onLayout = () => {
-    const { width, height } = Dimensions.get('window');
-    this.state.animator.updateFinalDimension('width', width);
-    this.state.animator.updateFinalDimension('height', this.getFlashcardContentHeight(height));
+    this.setState({ animator: new Animator(this.props.animator)});
+    this.state.animator.resetAnimations()
   }
 
   render = () => {
     return (
-      <View onLayout={this.onLayout} style={[this.layoutStyle, {width: '100%', height: '100%'}]}>
-        <Animated.View style=
-                         { [
-                           this.layoutStyle,
-                           { backgroundColor: 'transparent', shadowColor: 'black', shadowOffset: {width: 4, height: 4} },
-                           this.state.animator.getStyle()]
-                         }>
-          <TouchableWithoutFeedback onPress={() => { this.state.animator.startAnimations(true) }}>
+      <View style={[ this.layoutStyle, { width: '100%', height: '100%' } ]}>
+        <Animated.View style={[ this.layoutStyle, this.props.style, this.state.animator.getStyle() ]}>
+          <TouchableWithoutFeedback onPress={() => { this.state.animator.startAnimations() }}>
             <Image
-              style={{width: '100%', height: '100%'}}
-              source={{uri: this.imageProperties.source}}
-              resizeMode="contain"
+              style={{ width: '100%', height: '100%' }}
+              source={{ uri: this.props.imageProperties.source }}
+              resizeMode={ this.props.imageProperties.resizeMode }
             />
           </TouchableWithoutFeedback>
         </Animated.View>
