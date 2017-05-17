2f8010bae6ab04ae13498e18fde405f08a50ff21
Andżelo <andre2go@gmail.com>
2017-05-17 17:48:22 +0200

    Use resetStore function of Apollo

--- mobileClient/components/Home.js
+++ mobileClient/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
 import { Link } from 'react-router-native'
 import { connect } from 'react-redux'
-import { compose, graphql } from 'react-apollo'
+import { withApollo, compose, graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import update from 'immutability-helper'
 
@@ -17,6 +17,9 @@ class Home extends React.Component {
   logout = () => {
     console.log('* LOG * Home logout')
     this.props.logout()
+      .then(() => {
+          this.props.client.resetStore()
+        })
   }
 
   render () {
@@ -68,6 +71,7 @@ const logOutQuery = gql`
 
 export default compose(
   connect(),
+  withApollo,
   graphql(logOutQuery, {
     props: ({ ownProps, mutate }) => ({
       logout: () => mutate({
