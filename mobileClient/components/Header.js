7a975d055c80c6c22691f173ea694f591af15791
Andżelo <andre2go@gmail.com>
2017-05-23 16:40:30 +0200

    Improve styling of the Header component and add menu icon

--- mobileClient/components/Header.js
+++ mobileClient/components/Header.js
@@ -1,27 +1,56 @@
 import React from 'react'
-import { View } from 'react-native'
+import { withRouter } from 'react-router'
+import { View, TouchableOpacity } from 'react-native'
 import SvgUri from 'react-native-svg-uri'
 
-const logoBig = require('../images/logoBig.svg')
+import Hamburger from 'react-native-hamburger'
+
+import logoBig from '../images/logoBig.svg'
 import styles from '../styles/styles'
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+  }
+
+  goHome = () => {
+    this.props.history.push('/')
+  }
+
+  hideMenu = () => {
+    this.setState({ active: !this.state.active })
+  }
+
   render () {
     return (
       <View style={styles.header}>
-        <SvgUri
-          style={{
-            width: 170,
-            height: 45,
-            transform: [
-              { scale: 0.84 }
-            ]
-          }}
-          width='250'
-          height='65'
-          source={ logoBig }
-        />
+        <TouchableOpacity onPress={this.goHome}>
+          <SvgUri
+            style={{
+              padding: 0,
+              marginLeft: -10,
+              marginTop: -25,
+              marginBottom: 0,
+              width: 250,
+              height: 65,
+              flex: 1,
+              transform: [
+                { scale: 0.83 }
+              ]
+            }}
+            width='250'
+            height='65'
+            source={ logoBig }
+          />
+        </TouchableOpacity>
+
+        <Hamburger active={this.state.active} color='#662e8f' type='spinCross' onPress={this.hideMenu} style={{ marginTop: 30, flex: 1 }} />
       </View>
     )
   }
 }
+
+export default withRouter(Header)
