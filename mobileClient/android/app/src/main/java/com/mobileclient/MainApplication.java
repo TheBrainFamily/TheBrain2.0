f13015947b275e657e28f8881ccb289f26506e18
Przemysław Gierski <pgierski@o2.pl>
2017-05-23 19:29:20 +0200

    fonts

--- mobileClient/android/app/src/main/java/com/mobileclient/MainApplication.java
+++ mobileClient/android/app/src/main/java/com/mobileclient/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileclient;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.magus.fblogin.FacebookLoginPackage;
 import com.horcrux.svg.SvgPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FacebookLoginPackage(),
             new SvgPackage(),
             new ReactNativeYouTube()
       );
--- mobileClient/components/HeaderQuestion.js
+++ mobileClient/components/HeaderQuestion.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { Link } from 'react-router-native'
 import SvgUri from 'react-native-svg-uri'
+import Hamburger from 'react-native-hamburger'
 
 import styles from '../styles/styles'
 
 export default class HeaderQuestion extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+  }
+
+  hideMenu = () => {
+    this.setState({active: !this.state.active})
+  }
+
   render () {
     return (
       <View style={styles.questionHeader}>
         <View style={styles.questionHeaderFluxContainer}>
-          <SvgUri width="80" height="61" source={require('../images/logo.svg')}/>
-          <Text style={styles.headerTitle}>Chemistry</Text>
+          <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 14}}>
+            <SvgUri width="100" height="49" source={require('../images/logo.svg')}/>
+            <View style={styles.headerBorder}><Text style={styles.headerTitle}>Chemistry</Text></View>
+          </View>
+          <View style={{marginRight: 25}}>
+            <Hamburger active={this.state.active} color='#ffffff' type='spinCross' onPress={this.hideMenu} />
+          </View>
         </View>
       </View>
     )
