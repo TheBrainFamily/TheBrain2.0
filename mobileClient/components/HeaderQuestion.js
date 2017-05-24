125cc2c423f4f1ed9aca035a8d8c8ec5ef041354
Przemysław Gierski <pgierski@o2.pl>
2017-05-24 16:16:50 +0200

    navigation

--- mobileClient/components/HeaderQuestion.js
+++ mobileClient/components/HeaderQuestion.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { withRouter } from 'react-router'
+import { Text, View, TouchableOpacity } from 'react-native'
 import SvgUri from 'react-native-svg-uri'
 import Hamburger from 'react-native-hamburger'
 
 import styles from '../styles/styles'
 
-export default class HeaderQuestion extends React.Component {
+class HeaderQuestion extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -17,19 +18,27 @@ export default class HeaderQuestion extends React.Component {
     this.setState({active: !this.state.active})
   }
 
+  goHome = () => {
+    this.props.history.push('/')
+  }
+
   render () {
     return (
       <View style={styles.questionHeader}>
         <View style={styles.questionHeaderFluxContainer}>
           <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 14}}>
-            <SvgUri width="100" height="49" source={require('../images/logo.svg')}/>
+            <TouchableOpacity onPress={this.goHome}>
+              <SvgUri width="100" height="49" source={require('../images/logo.svg')}/>
+            </TouchableOpacity>
             <View style={styles.headerBorder}><Text style={styles.headerTitle}>Chemistry</Text></View>
           </View>
           <View style={{marginRight: 25}}>
-            <Hamburger active={this.state.active} color='#ffffff' type='spinCross' onPress={this.hideMenu} />
+            <Hamburger active={this.state.active} color='#ffffff' type='spinCross' onPress={this.hideMenu}/>
           </View>
         </View>
       </View>
     )
   }
 }
+
+export default withRouter(HeaderQuestion)
