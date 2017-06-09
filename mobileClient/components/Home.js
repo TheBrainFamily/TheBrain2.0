e18bed8252ea492e215f85b31675d64e1d2bf0d5
Andżelo <andre2go@gmail.com>
2017-06-09 15:32:42 +0200

    Add menu overlay

--- mobileClient/components/Home.js
+++ mobileClient/components/Home.js
@@ -1,102 +1,28 @@
 import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-import { Link } from 'react-router-native'
-import { connect } from 'react-redux'
-import { withApollo, compose, graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import update from 'immutability-helper'
-import { FBLoginManager } from 'react-native-facebook-login'
+import { Text, View } from 'react-native'
 
 import Header from './Header'
 import Lecture from './Lecture'
 
 import styles from '../styles/styles'
 
-import currentUserQuery from '../../client/shared/graphql/queries/currentUser'
-
-class Home extends React.Component {
-  logout = () => {
-    this.props.logout()
-      .then(() => {
-        FBLoginManager.getCredentials((error, data) => {
-          if (!error && data && data.credentials) {
-            FBLoginManager.logout(() => {}) // any callback is required
-          }
-        })
-        this.props.client.resetStore()
-      })
-  }
-
+export default class Home extends React.Component {
   render () {
-    if (this.props.data.loading) {
-      return <View />
-    }
-
-    const currentUser = this.props.data.CurrentUser
-    const activated = currentUser && currentUser.activated
-
     return (
       <View>
-        <Header withShadow={true} />
-
-        {activated
-          ? <View style={{ margin: 10 }}>
-            <TouchableOpacity onPress={this.logout}>
-              <Text style={styles.button}>Logout</Text>
-            </TouchableOpacity>
-          </View>
-          : <View style={{ margin: 10 }}>
-            <Link to='/login'>
-              <Text style={styles.button}>Login</Text>
-            </Link>
-            <Link to='/questions'>
-              <Text style={styles.button}>Questions</Text>
-            </Link>
-          </View>
-        }
+        <Header withShadow />
 
         <View style={{ alignItems: 'center' }}>
-          <Lecture />
-
           <Text style={[ styles.textDefault, {
-            marginTop: 35,
+            margin: 30,
             width: 200
           }]}>
             Watch video and wait for the question
           </Text>
+
+          <Lecture />
         </View>
       </View>
     )
   }
 }
-
-const logOutQuery = gql`
-    mutation logOut {
-        logOut {
-            _id, username, activated
-        }
-    }
-`
-
-export default compose(
-  connect(),
-  withApollo,
-  graphql(logOutQuery, {
-    props: ({ ownProps, mutate }) => ({
-      logout: () => mutate({
-        updateQueries: {
-          CurrentUser: (prev, { mutationResult }) => {
-            console.log('Gozdecki: mutationResult', mutationResult)
-            console.log('Gozdecki: prev', prev)
-            return update(prev, {
-              CurrentUser: {
-                $set: mutationResult.data.logOut
-              }
-            })
-          }
-        }
-      })
-    })
-  }),
-  graphql(currentUserQuery)
-)(Home)
