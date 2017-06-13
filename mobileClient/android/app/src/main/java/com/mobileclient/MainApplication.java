d259baf44219272fcb8a888ce18ca1864e60110e
Andżelo <andre2go@gmail.com>
2017-06-13 17:22:01 +0200

    Link missing React Native modules

--- mobileClient/android/app/src/main/java/com/mobileclient/MainApplication.java
+++ mobileClient/android/app/src/main/java/com/mobileclient/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileclient;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import com.horcrux.svg.SvgPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new FacebookLoginPackage(),
             new SvgPackage(),
             new ReactNativeYouTube()
