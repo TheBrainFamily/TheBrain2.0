7c7f9320a56ac020627bc0fc9a6fd8e1216866f0
Andżelo <andre2go@gmail.com>
2017-06-16 17:20:34 +0200

    Improve the onPress animation for the circle button

--- mobileClient/components/Home.js
+++ mobileClient/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Animated } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { withRouter } from 'react-router'
 
 import Header from './Header'
