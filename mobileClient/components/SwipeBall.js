b7edc205ff682bf3c07eeb9a0c367b0fba584226
Andżelo <andre2go@gmail.com>
2017-06-20 17:46:41 +0200

    Use PanResponder class to add drag and move animation for the SwipeBall component

--- mobileClient/components/SwipeBall.js
+++ mobileClient/components/SwipeBall.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View } from 'react-native'
+import { Animated, PanResponder, View } from 'react-native'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import update from 'immutability-helper'
@@ -12,81 +12,92 @@ import { getSwipeDirection, getDragLength, getDirectionEvaluationValue } from '.
 
 import sessionCountQuery from '../../client/shared/graphql/queries/sessionCount'
 
+const defaultBallColors = ['#7c45d2', '#672f92']
+const alternativeBallColors = ['#62c46c', '#3f873f']
+
 class SwipeBall extends React.Component {
-  constructor (props) {
+  constructor(props){
     super(props)
+
     this.state = {
-      position: {
-        x: 0,
-        y: 0
-      }
+      pan: new Animated.ValueXY(),
+      ballColors: defaultBallColors
     }
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, gesture) => {
+        const delta = { dx: 0, dy: 0 }
+        const isHorizontalMove = Math.abs(gesture.dx) > Math.abs(gesture.dy)
+        if (isHorizontalMove) {
+          delta.dx =  this.state.pan.x
+        } else {
+          delta.dy =  this.state.pan.y
+        }
+        Animated.event([null, delta])(e, gesture)
+      },
+      onPanResponderRelease: (e, gesture) => {
+        const dragLen = getDragLength(gesture.dx, gesture.dy)
+        if (dragLen > 40) {
+          const isHorizontalMove = Math.abs(gesture.dx) > Math.abs(gesture.dy)
+          const targetPosition = { x: 0, y: 0 }
+          if (isHorizontalMove) {
+            targetPosition.x = 140 * Math.sign(gesture.dx)
+          } else {
+            targetPosition.y = 110 * Math.sign(gesture.dy)
+          }
+
+          setTimeout(() => {
+            this.setState({ ballColors: alternativeBallColors })
+          }, 200)
+          Animated.spring(this.state.pan, {
+            toValue: targetPosition
+          }).start(this.submitEvaluation)
+        } else {
+          this.resetPosition()
+        }
+      }
+    })
+  }
+
+  resetPosition = (cb = () => {}) => {
+    Animated.spring(this.state.pan, {
+      toValue: { x: 0, y: 0 }
+    }).start(cb)
+  }
+
+  submitEvaluation = () => {
+    console.log('* LOG * submitEvaluation')
+    // this.setState({ ballColors: alternativeBallColors })
+    const direction = getSwipeDirection(this.state.pan.x._value, this.state.pan.y._value)
+    const evaluationValue = getDirectionEvaluationValue(direction)
+    this.onSubmitEvaluation(evaluationValue)
   }
 
   onSubmitEvaluation = (value) => {
-    this.props.submit({
-      itemId: this.props.evalItemId,
-      evaluation: value
+    this.resetPosition(() => {
+      this.props.submit({
+        itemId: this.props.evalItemId,
+        evaluation: value
+      })
+      this.props.dispatch(updateAnswerVisibility(false))
+      this.setState({ ballColors: defaultBallColors })
     })
-    this.props.dispatch(updateAnswerVisibility(false))
-    // this.props.flipCardCb()
-  }
-
-  isDragLongEnough = () => {
-    const dragLen = getDragLength(this.state.position.x, this.state.position.y)
-    return dragLen > 100
-  }
-
-  resetPosition = (e) => {
-    const direction = getSwipeDirection(this.state.position.x, this.state.position.y)
-    if (this.isDragLongEnough()) {
-      const evaluationValue = getDirectionEvaluationValue(direction)
-      this.onSubmitEvaluation(evaluationValue)
-    }
-    // Reset on release
-    this.setState({ position: { x: 0, y: 0 } })
-    // const baseSwipeValue = 'left'
-    // const baseDrag = 0
-    // this.props.updateSwipeStateCb(baseSwipeValue, baseDrag)
-  }
-
-  setupInitialDrag = (e) => {
-    this.drag = {
-      x: e.nativeEvent.pageX,
-      y: e.nativeEvent.pageY
-    }
-  }
-
-  setPosition = (event) => {
-    // Update our state with the deltaX/deltaY of the movement
-    const x = this.state.position.x + (event.nativeEvent.pageX - this.drag.x)
-    const y = this.state.position.y + (event.nativeEvent.pageY - this.drag.y)
-    this.setState({ position: { x, y } })
-    // const dragLen = getDragLength(this.state.position.x, this.state.position.y)
-    // const swipeDirection = getSwipeDirection(this.state.position.x, this.state.position.y)
-    // this.props.updateSwipeStateCb(swipeDirection, dragLen)
-
-    // Set our drag to be the new position so our delta can be calculated next time correctly
-    this.drag.x = event.nativeEvent.pageX
-    this.drag.y = event.nativeEvent.pageY
-  }
-
-  getCardTransformation = function () {
-    const transform = [{ translateX: this.state.position.x }, { translateY: this.state.position.y - 35 }]
-    return { transform }
   }
 
   render () {
     return (
-      <LinearGradient
-        colors={['#7c45d2', '#672f92']}
-        style={[ styles.answerSwipeBall, this.getCardTransformation() ]}
-        onResponderMove={this.setPosition}
-        onResponderRelease={this.resetPosition}
-        onStartShouldSetResponder={() => true}
-        onMoveShouldSetResponder={() => true}
-        onResponderGrant={this.setupInitialDrag}
-      />
+      <View style={styles.draggableContainer}>
+        <Animated.View
+          {...this.panResponder.panHandlers}
+          style={[ styles.answerSwipeBall, this.state.pan.getLayout() ]}
+        >
+          <LinearGradient
+            style={styles.answerSwipeBall}
+            colors={this.state.ballColors}
+          />
+        </Animated.View>
+      </View>
     )
   }
 }
