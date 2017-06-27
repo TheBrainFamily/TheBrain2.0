0b41dc9d6e6b4340aaf761c71218f2f67862ad7c
Andżelo <andre2go@gmail.com>
2017-06-27 14:16:35 +0200

    Add a Course component to the web version

---
+++ client/src/components/Course.js
@@ -0,0 +1,41 @@
+// @flow
+
+import React from 'react'
+import { compose, graphql } from 'react-apollo'
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+
+import currentItemsExistQuery from '../../shared/graphql/queries/currentItemsExist'
+
+class Course extends React.Component {
+  componentWillReceiveProps (nextProps) {
+    if (!nextProps.data || nextProps.data.loading || nextProps.data.error) {
+      return
+    }
+
+    if (nextProps.data.ItemsWithFlashcard.length > 0) {
+      nextProps.dispatch(push('/questions'))
+    } else {
+      nextProps.dispatch(push('/lecture'))
+    }
+  }
+
+  render () {
+    if (this.props.data.loading) {
+      return (<p>Loading...</p>)
+    }
+
+    if (this.props.data.error) {
+      return (<p>Error...</p>)
+    }
+
+    return <div />
+  }
+}
+
+export default compose(
+  connect(),
+  withRouter,
+  graphql(currentItemsExistQuery)
+)(Course)
