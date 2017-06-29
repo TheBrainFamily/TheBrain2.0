652333872b5835547bde221bafcbbd1493b151ab
Andżelo <andre2go@gmail.com>
2017-06-29 14:31:52 +0200

    Add support for multiple courses in Home, Header, Course and Lecture components

--- client/src/App.js
+++ client/src/App.js
@@ -26,8 +26,8 @@ class App extends Component {
             <Header />
             <div className='App-intro styleIntroduction'>
               <Route exact key='Home' path='/' component={Home} />
-              <Route key='Course' path='/course' component={Course} />
-              <Route exact key='Lecture' path='/lecture' component={Lecture} />
+              <Route key='Course' path='/course/:courseId' component={Course} />
+              <Route key='Lecture' path='/lecture/:courseId' component={Lecture} />
               <Route exact key='wellDone' path='/wellDone' component={WellDone} />
               <Route exact key='questions' path='/questions' component={Questions} />
               <Route exact key='login' path='/login' component={Login} />
