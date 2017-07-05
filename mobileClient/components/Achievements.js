0c54aa4afc72c0d567048011bf7638f3256e0a10
Andżelo <andre2go@gmail.com>
2017-07-05 17:25:24 +0200

    Improve Achievements component

--- mobileClient/components/Achievements.js
+++ mobileClient/components/Achievements.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { connect } from 'react-redux'
 
 import Header from './Header'
 import PageTitle from './PageTitle'
+
+import styles from '../styles/styles'
 
 class Achievements extends React.Component {
   render () {
@@ -15,6 +17,42 @@ class Achievements extends React.Component {
         <Header />
 
         <PageTitle text='ACHIEVEMENTS LIST' />
+        <View style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: '100%',
+          height: '100%',
+          padding: 20
+        }}>
+          <View style={{ width: '50%', padding: 30 }}>
+            <Image
+              style={{ width: '100%', height: 220 }}
+              source={require('../images/BillyBaby.png')}
+            />
+            <Text style={[styles.textDefault, { color: '#6905ea' }]}>Billy Baby</Text>
+          </View>
+          <View style={{ width: '50%', padding: 30 }}>
+            <Image
+              style={{ width: 120, height: 220 }}
+              source={require('../images/BillyBaby.png')}
+            />
+            <Text style={[styles.textDefault, { color: '#6905ea' }]}>Billy Junior</Text>
+          </View>
+          <View style={{ width: '50%', padding: 30 }}>
+            <Image
+              style={{ width: 120, height: 220 }}
+              source={require('../images/BillyBaby.png')}
+            />
+            <Text style={[styles.textDefault, { color: '#6905ea' }]}>Billy Teen</Text>
+          </View>
+          <View style={{ width: '50%', padding: 30 }}>
+            <Image
+              style={{ width: 120, height: 220 }}
+              source={require('../images/BillyBaby.png')}
+            />
+            <Text style={[styles.textDefault, { color: '#6905ea' }]}>Big Billy</Text>
+          </View>
+        </View>
       </View>
     )
   }
