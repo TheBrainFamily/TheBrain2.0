2503a8a23017921b90eda78c0949bcfc764cc8f6
Andżelo <andre2go@gmail.com>
2017-07-06 14:53:32 +0200

    Display Lecture video placeholders on the Lectures List page

--- mobileClient/components/Lectures.js
+++ mobileClient/components/Lectures.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import { connect } from 'react-redux'
 
 import Header from './Header'
 import PageTitle from './PageTitle'
+import { LectureVideoWithRouter } from './Lecture'
 
 class Lectures extends React.Component {
   render () {
@@ -15,8 +16,57 @@ class Lectures extends React.Component {
         <Header />
 
         <PageTitle text='LECTURES LIST' />
+        <View style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: '100%',
+          height: '100%',
+          padding: 10
+        }}>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+          <View style={{ width: '50%', padding: 10 }}>
+            <LectureVideoWithRouter lesson={{}} height={100} />
+            <Text style={style.title}>"Very long lecture title"</Text>
+          </View>
+        </View>
       </View>
     )
+  }
+}
+
+const style = {
+  title: {
+    color: '#999',
+    fontSize: 12,
+    fontFamily: 'Exo2-Regular',
+    textAlign: 'center'
   }
 }
 
