0f57394ea388eec542bbfb00e53893b72eb0517d
Tomasz Wilczek <twilczek@thebrain.pro>
2017-07-07 17:52:38 +0200

    Use FlatList React Native component to render list of lectures for a selected course

--- mobileClient/components/Lectures.js
+++ mobileClient/components/Lectures.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+import { graphql, compose } from 'react-apollo'
 import { connect } from 'react-redux'
 
 import Header from './Header'
 import PageTitle from './PageTitle'
-import { LectureVideoWithRouter } from './Lecture'
+import Video from './Video'
+
+import lessonsQuery from '../../client/shared/graphql/queries/lessons'
 
 class Lectures extends React.Component {
+  renderLecture = ({ item }) => {
+    return (
+      <View style={{ width: '50%', padding: 10 }} key={item._id}>
+        <Video videoId={item.youtubeId} height={100}/>
+        <Text style={style.title}>{item.description}</Text>
+      </View>
+    )
+  }
+
   render () {
+    if (this.props.lessons.loading) {
+      return <View />
+    }
     return (
       <View style={{
         height: '100%',
@@ -15,47 +30,15 @@ class Lectures extends React.Component {
       }}>
         <Header />
 
-        <PageTitle text='LECTURES LIST' />
-        <View style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          width: '100%',
-          height: '100%',
-          padding: 10
-        }}>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-          <View style={{ width: '50%', padding: 10 }}>
-            <LectureVideoWithRouter lesson={{}} height={100} />
-            <Text style={style.title}>"Very long lecture title"</Text>
-          </View>
-        </View>
+        <PageTitle text='LECTURES LIST'/>
+        <FlatList
+          data={this.props.lessons.Lessons}
+          renderItem={this.renderLecture}
+          style={{
+            paddingHorizontal: 10
+          }}
+          numColumns={2}
+        />
       </View>
     )
   }
@@ -70,4 +53,21 @@ const style = {
   }
 }
 
-export default connect()(Lectures)
+const mapStateToProps = (state) => {
+  return {
+    selectedCourse: state.course.selectedCourse
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  graphql(lessonsQuery, {
+    name: 'lessons',
+    options: (ownProps) => {
+      const courseId = ownProps.selectedCourse._id
+      return {
+        variables: { courseId }
+      }
+    }
+  })
+)(Lectures)
