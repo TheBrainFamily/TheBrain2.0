a2a368b57940a3241f051883e3d9535c3fe6c42f
lolezy <mopidowicz@thebrain.pro>
2017-07-14 15:29:36 +0200

    created CourseIcon component, added assets, added CourseIcon to Home and Lecture components

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -8,7 +8,6 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 
 import Flashcard from './Flashcard'
-import SessionSummary from './SessionSummary'
 import currentUserQuery from '../../shared/graphql/queries/currentUser'
 import sessionCountQuery from '../../shared/graphql/queries/sessionCount'
 
