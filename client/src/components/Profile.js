fdac1ff0e3169027561d80f0d6388449e653168a
Tomasz Wilczek <twilczek@thebrain.pro>
2017-07-20 18:00:40 +0200

    Handle password change - WIP

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,53 +4,69 @@ import React from 'react'
 import { compose, graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 import FlexibleContentWrapper from './FlexibleContentWrapper'
 
 class Profile extends React.Component {
   state = {
-    error: ''
+    error: '',
+    isValid: false
   }
 
   submit = (e) => {
     e.preventDefault()
-    this.setState({ error: '' })
-
-    console.log('* LOG * submit', e)
+    swal("Good job!", "Password changed successfully", "success")
+    // this.setState({ error: '' })
+    // this.props.submit({ oldPassword: this.refs.oldPassword.value, newPassword: this.refs.newPassword.value })
+    //   .then(() => {
+    //       this.props.dispatch(push('/')
+    //   })
+    //   .catch((data) => {
+    //     // const error = data.graphQLErrors[0].message
+    //     const error = "Byle jaki tekst"
+    //     this.setState({ error })
+    //   })
   }
 
   comparePasswords = () => {
-    console.log('* LOG * comparePasswords')
     if (this.refs.newPassword.value.length !== this.refs.newPasswordConfirmation.value.length) {
-      return this.setState({ error: '' })
+      return this.setState({ error: '', isValid: false })
+    }
+    if (this.refs.newPassword.value !== this.refs.newPasswordConfirmation.value) {
+      return this.setState({ error: 'Passwords don\'t match', isValid: false })
+    }
+    if (this.refs.newPasswordConfirmation.value.length > 3) {
+      return this.setState({ isValid: true })
     }
 
-    if (this.refs.newPassword.value !== this.refs.newPasswordConfirmation.value) {
-      return this.setState({ error: 'Passwords don\'t match' })
-    }
   }
 
   render () {
     return (
       <FlexibleContentWrapper offset={400}>
         <form className='form' onSubmit={this.submit}>
-          {this.state.error &&
-            <div className='text-error'>{ this.state.error }</div>
-          }
+          <div className={!this.state.error ? 'hidden' : null}>
+            <p className='alert-error'>{ this.state.error }</p>
+          </div>
           <div>
             <label>Old Password:</label>
-            <input ref='oldPassword' type='password' name='oldPassword' />
+            <input ref='oldPassword' type='password' name='oldPassword'/>
           </div>
           <div>
             <label>New Password:</label>
-            <input ref='newPassword' type='password' name='newPassword' />
+            <input ref='newPassword' type='password' name='newPassword' onChange={this.comparePasswords}/>
           </div>
           <div>
             <label>Confirm New Password:</label>
-            <input ref='newPasswordConfirmation' type='password' name='newPasswordConfirmation' onChange={this.comparePasswords} />
+            <input className={!!this.state.error ? 'error' : null} ref='newPasswordConfirmation' type='password'
+                   name='newPasswordConfirmation'
+                   onChange={this.comparePasswords}/>
           </div>
           <div>
-            <input type='submit' value='Change Password' />
+            <input type='submit' value='Change Password' disabled={!this.state.isValid || !!this.state.error}/>
           </div>
         </form>
       </FlexibleContentWrapper>
@@ -67,7 +83,7 @@ const changePasswordMutation = gql`
 export default compose(
   connect(),
   graphql(changePasswordMutation, {
-    props: ({ mutate }) => ({
+    refs: ({ mutate }) => ({
       submit: ({ oldPassword, newPassword }) => mutate({
         variables: {
           oldPassword,
