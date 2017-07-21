04a2120cbb438aa9db9803fba37bd1340ee0df8e
Przemysław Gierski <pgierski@o2.pl>
2017-07-21 17:35:29 +0200

    fix not changing text of card after showing answer

--- mobileClient/components/Card.js
+++ mobileClient/components/Card.js
@@ -10,15 +10,15 @@ export default class Card extends React.Component {
       <View style={[styles.flipCardContent, this.props.dynamicStyles.content]}>
         <View style={{ display: this.props.visibleAnswer ? 'none' : 'flex' }}>
           <Text
-            style={[styles.flipCardHeader, { color: this.props.visibleAnswer ? 'white' : '#662d91' }]}>QUESTION</Text>
+            style={styles.flipCardHeader}>{!this.props.visibleAnswer && 'QUESTION'}</Text>
           <View style={styles.flipCardBody}>
-            <Text style={styles.primaryText}>{this.props.question}</Text>
+            <Text style={styles.primaryText}>{!this.props.visibleAnswer && this.props.question}</Text>
           </View>
         </View>
-        <View style={{ transform: [{ rotateY: '180deg' }], display: this.props.visibleAnswer ? 'flex' : 'none' }}>
+        <View style={{ transform: [{ rotateY: '180deg' }], display: !this.props.visibleAnswer ? 'none' : 'flex' }}>
           <Text style={styles.flipCardHeader}>ANSWER</Text>
           <View style={styles.flipCardBody}>
-            <Text style={styles.primaryText}>{this.props.answer}</Text>
+            <Text style={styles.primaryText}>{this.props.visibleAnswer && this.props.answer}</Text>
           </View>
         </View>
       </View>
