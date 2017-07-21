d8b70ed0ab1af1fe1888ae9936012520c50cd5e8
Andżelo <andre2go@gmail.com>
2017-07-21 15:47:27 +0200

    Improve password validation in the Profile component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,37 +6,46 @@ import gql from 'graphql-tag'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import swal from 'sweetalert2'
+
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 import FlexibleContentWrapper from './FlexibleContentWrapper'
 
 class Profile extends React.Component {
   state = {
-    error: '',
+    oldPasswordError: '',
+    confirmationError: '',
     isValid: false
+  }
+
+  goHome = () => {
+    this.props.dispatch(push('/'))
   }
 
   submit = (e) => {
     e.preventDefault()
-    swal("Good job!", "Password changed successfully", "success")
-    // this.setState({ error: '' })
-    // this.props.submit({ oldPassword: this.refs.oldPassword.value, newPassword: this.refs.newPassword.value })
-    //   .then(() => {
-    //       this.props.dispatch(push('/')
-    //   })
-    //   .catch((data) => {
-    //     // const error = data.graphQLErrors[0].message
-    //     const error = "Byle jaki tekst"
-    //     this.setState({ error })
-    //   })
+    this.props.submit({ oldPassword: this.refs.oldPassword.value, newPassword: this.refs.newPassword.value })
+      .then(() => {
+        swal("Good job!", "Password changed successfully", "success").then(this.goHome, this.goHome)
+      })
+      .catch((data) => {
+        const oldPasswordError = data.graphQLErrors[0].message
+        this.setState({ oldPasswordError })
+      })
   }
 
-  comparePasswords = () => {
+  validatePasswords = () => {
+    if (!this.refs.oldPassword.value.length) {
+      this.setState({ oldPasswordError: 'Password cannot be empty', isValid: false })
+    } else {
+      this.setState({ oldPasswordError: '' })
+    }
+
     if (this.refs.newPassword.value.length !== this.refs.newPasswordConfirmation.value.length) {
-      return this.setState({ error: '', isValid: false })
+      return this.setState({ confirmationError: '', isValid: false })
     }
     if (this.refs.newPassword.value !== this.refs.newPasswordConfirmation.value) {
-      return this.setState({ error: 'Passwords don\'t match', isValid: false })
+      return this.setState({ confirmationError: 'Passwords don\'t match', isValid: false })
     }
     if (this.refs.newPasswordConfirmation.value.length > 3) {
       return this.setState({ isValid: true })
@@ -45,28 +54,36 @@ class Profile extends React.Component {
   }
 
   render () {
+    const error = this.state.oldPasswordError || this.state.confirmationError
+
     return (
       <FlexibleContentWrapper offset={400}>
         <form className='form' onSubmit={this.submit}>
-          <div className={!this.state.error ? 'hidden' : null}>
-            <p className='alert-error'>{ this.state.error }</p>
+          <div className={!error ? 'hidden' : null}>
+            <p className='alert-error'>{ error }</p>
           </div>
           <div>
             <label>Old Password:</label>
-            <input ref='oldPassword' type='password' name='oldPassword'/>
+            <input className={!!this.state.oldPasswordError ? 'error' : null} ref='oldPassword'
+                   type='password'
+                   name='oldPassword'
+                   onChange={this.validatePasswords}
+            />
           </div>
           <div>
             <label>New Password:</label>
-            <input ref='newPassword' type='password' name='newPassword' onChange={this.comparePasswords}/>
+            <input ref='newPassword' type='password' name='newPassword' onChange={this.validatePasswords}/>
           </div>
           <div>
             <label>Confirm New Password:</label>
-            <input className={!!this.state.error ? 'error' : null} ref='newPasswordConfirmation' type='password'
+            <input className={!!this.state.confirmationError ? 'error' : null} ref='newPasswordConfirmation'
+                   type='password'
                    name='newPasswordConfirmation'
-                   onChange={this.comparePasswords}/>
+                   onChange={this.validatePasswords}
+            />
           </div>
           <div>
-            <input type='submit' value='Change Password' disabled={!this.state.isValid || !!this.state.error}/>
+            <input type='submit' value='Change Password' disabled={!this.state.isValid || !!error}/>
           </div>
         </form>
       </FlexibleContentWrapper>
@@ -75,15 +92,17 @@ class Profile extends React.Component {
 }
 
 const changePasswordMutation = gql`
-    mutation changePassword($oldPassword: String!, $newPassword: String!){
-        changePassword(oldPassword: $oldPassword, newPassword: $newPassword)
+    mutation changePassword($oldPassword: String!, $newPassword: String!) {
+        changePassword(oldPassword: $oldPassword, newPassword: $newPassword) {
+          success
+        }
     }
 `
 
 export default compose(
-  connect(),
+  connect(state => state),
   graphql(changePasswordMutation, {
-    refs: ({ mutate }) => ({
+    props: ({ mutate }) => ({
       submit: ({ oldPassword, newPassword }) => mutate({
         variables: {
           oldPassword,
