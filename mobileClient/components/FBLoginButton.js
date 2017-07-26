2c985aa79a8932c8339643e537a604e022231369
Przemysław Gierski <pgierski@o2.pl>
2017-07-26 18:02:52 +0200

    fix login with facebook button height and linting

--- mobileClient/components/FBLoginButton.js
+++ mobileClient/components/FBLoginButton.js
@@ -8,41 +8,41 @@ import { withApollo, graphql } from 'react-apollo'
 class FBLoginButton extends React.Component {
   render () {
     return (
-      <FBLogin style={{ marginVertical: 10 }}
-        ref={(fbLogin) => {
-          this.fbLogin = fbLogin
-        }}
-        permissions={['email']}
-        loginBehavior={FBLoginManager.LoginBehaviors.Native}
-        onLogin={(data) => {
-          console.log('Logged in!')
-          this.props.logInWithFacebook({ accessToken: data.credentials.token })
-          this.props.history.push('/')
-        }}
-        onLogout={() => {
-          console.log('Logged out.')
-          this.props.client.resetStore()
-          this.props.history.push('/')
-        }}
-        onLoginFound={(data) => {
-          console.log('Existing login found.')
-          console.log(data)
-          this.props.logInWithFacebook({ accessToken: data.credentials.token })
-        }}
-        onLoginNotFound={() => {
-          console.log('No user logged in.')
-        }}
-        onError={(data) => {
-          console.log('ERROR')
-          console.log(data)
-        }}
-        onCancel={() => {
-          console.log('User cancelled.')
-        }}
-        onPermissionsMissing={(data) => {
-          console.log('Check permissions!')
-          console.log(data)
-        }}
+      <FBLogin style={{ marginVertical: 10, maxHeight: 60 }}
+               ref={(fbLogin) => {
+                 this.fbLogin = fbLogin
+               }}
+               permissions={['email']}
+               loginBehavior={FBLoginManager.LoginBehaviors.Native}
+               onLogin={(data) => {
+                 console.log('Logged in!')
+                 this.props.logInWithFacebook({ accessToken: data.credentials.token })
+                 this.props.history.push('/')
+               }}
+               onLogout={() => {
+                 console.log('Logged out.')
+                 this.props.client.resetStore()
+                 this.props.history.push('/')
+               }}
+               onLoginFound={(data) => {
+                 console.log('Existing login found.')
+                 console.log(data)
+                 this.props.logInWithFacebook({ accessToken: data.credentials.token })
+               }}
+               onLoginNotFound={() => {
+                 console.log('No user logged in.')
+               }}
+               onError={(data) => {
+                 console.log('ERROR')
+                 console.log(data)
+               }}
+               onCancel={() => {
+                 console.log('User cancelled.')
+               }}
+               onPermissionsMissing={(data) => {
+                 console.log('Check permissions!')
+                 console.log(data)
+               }}
       />
     )
   };
