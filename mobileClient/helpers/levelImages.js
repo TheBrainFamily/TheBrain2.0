7870bf4d47b598332dc218b92130204f811d50c2
Andżelo <andre2go@gmail.com>
2017-08-04 17:36:32 +0200

    Add levelImages helper

---
+++ mobileClient/helpers/levelImages.js
@@ -0,0 +1,10 @@
+export default {
+  1: { file: require('../images/TB_level1.png') },
+  2: { file: require('../images/TB_level2.png') },
+  3: { file: require('../images/TB_level3.png') },
+  4: { file: require('../images/TB_level4.png') },
+  5: { file: require('../images/TB_level5.png') },
+  6: { file: require('../images/TB_level6.png') },
+  7: { file: require('../images/TB_level7.png') },
+  8: { file: require('../images/TB_level8.png') }
+}
