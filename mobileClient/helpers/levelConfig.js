8269a0468d629842b1516444758632f339d9bd37
Ponton <tgierczynski@thebrain.pro>
2017-08-08 14:15:15 +0200

    levelConfig fix and update

--- mobileClient/helpers/levelConfig.js
+++ mobileClient/helpers/levelConfig.js
@@ -1,11 +1,11 @@
 export default {
-  0: { file: require('../images/TB_level1.png'), name: 'infant' },
-  1: { file: require('../images/TB_level1.png'), name: 'baby' },
-  2: { file: require('../images/TB_level2.png'), name: 'youngster' },
-  3: { file: require('../images/TB_level3.png'), name: 'child' },
-  4: { file: require('../images/TB_level4.png'), name: 'teen' },
-  5: { file: require('../images/TB_level5.png'), name: 'student' },
-  6: { file: require('../images/TB_level6.png'), name: 'grownup' },
-  7: { file: require('../images/TB_level7.png'), name: 'specialist' },
-  8: { file: require('../images/TB_level8.png'), name: 'expert' }
+  0: { file: require('../images/TB_level1.png'), name: 'Infant' }, //level undefined Number bulletproofing
+  1: { file: require('../images/TB_level1.png'), name: 'Infant' },
+  2: { file: require('../images/TB_level2.png'), name: 'Baby' },
+  3: { file: require('../images/TB_level3.png'), name: 'Youngster' },
+  4: { file: require('../images/TB_level4.png'), name: 'Teen' },
+  5: { file: require('../images/TB_level5.png'), name: 'Student' },
+  6: { file: require('../images/TB_level6.png'), name: 'Grownup' },
+  7: { file: require('../images/TB_level7.png'), name: 'Specialist' },
+  8: { file: require('../images/TB_level8.png'), name: 'Expert' }
 }
