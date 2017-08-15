2b9a6758043b2bd0e24d4f1ff96043f762580d2d
lolezy <mopidowicz@thebrain.pro>
2017-08-15 18:06:41 +0200

    server side flashcard filtering, profile page casual mode switch WIP

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -100,14 +100,8 @@ class AppHeader extends React.Component {
                   <div className={'menu-separator menu-separator-visible'} />
                   <a>ACHIEVEMENTS LIST</a>
                   <div className={'menu-separator menu-separator-visible'} />
-                  { currentUser && !currentUser.facebookId
-                    ?
-                    <span>
-                      <a onClick={() => this.props.dispatch(push('/profile'))}>PROFILE</a>
-                      < div className={'menu-separator menu-separator-visible'} />
-                    </span>
-                    : null
-                  }
+                  <a onClick={() => this.props.dispatch(push('/profile'))}>PROFILE</a>
+                  < div className={'menu-separator menu-separator-visible'} />
                 </div>
                 }
                 <a onClick={() => this.props.dispatch(push('/contact'))}>CONTACT</a>
