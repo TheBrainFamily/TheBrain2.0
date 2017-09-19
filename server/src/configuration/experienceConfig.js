0df4266b4df36a2ce7c2cb9add48534d9a1c19cc
Jan Mozgawa <jmozgawa@Jans-MacBook-Pro.local>
2017-09-19 17:45:58 +0200

    amend level formula

--- server/src/configuration/experienceConfig.js
+++ server/src/configuration/experienceConfig.js
@@ -13,5 +13,5 @@ export function getExperienceForAction(action) {
 }
 
 export function calculateUserLevel(xp: Number) {
-  return Math.floor(0.25 * Math.sqrt(xp)) + 1
+  return Math.floor(0.2 * Math.sqrt(xp)) + 1
 }
