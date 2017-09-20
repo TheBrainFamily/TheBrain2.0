89d5b806382fdf4f8e6bc9ffc4720309a4f85e35
Jan Mozgawa <jmozgawa@Jans-MacBook-Pro.local>
2017-09-20 21:32:28 +0200

    remove lectures list from menu cause it is not yet implemented

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -104,7 +104,7 @@ class AppHeader extends React.Component {
                 }
                 {currentUser &&
                 <div>
-                  <a>LECTURES LIST</a>
+                  {/*<a>LECTURES LIST</a>*/}
                   <div className={'menu-separator menu-separator-visible'} />
                   <a onClick={() => this.props.dispatch(push('/calendar'))}>REVIEWS CALENDAR</a>
                   <div className={'menu-separator menu-separator-visible'} />
