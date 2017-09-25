6297bb3b071ca8d51c024d342c8cd89d9d629e0e
Ponton <tgierczynski@thebrain.pro>
2017-09-25 15:15:05 +0200

    change letter spacing for textDefault, align components in courseHeader

--- mobileClient/components/CourseHeader.js
+++ mobileClient/components/CourseHeader.js
@@ -35,7 +35,7 @@ class CourseHeader extends React.Component {
     return (
       <View style={[style.courseHeader, {backgroundColor: this.props.backgroundColor, height: this.props.height}]}>
         <View style={styles.questionHeaderFluxContainer}>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
             <TouchableOpacity onPress={this.closeCourse}>
               <Image
                 style={{ width: 100, height: 49 }}
