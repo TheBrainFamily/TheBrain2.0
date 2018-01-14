b1002a0e97a2131e1579f14d9eceae0c26f520d5
Artem Govorov <artem.govorov@gmail.com>
2018-01-14 13:51:21 +1000

    Update wallaby-web.js

--- wallaby-web.js
+++ wallaby-web.js
@@ -3,48 +3,30 @@ process.env.ENZYME=true
 process.env.NODE_ENV="TESTING"
 
 module.exports = (wallaby) => {
-  const path = require('path');
-
-  process.env.NODE_PATH +=
-    path.delimiter + path.join(wallaby.localProjectDir, 'node_modules') +
-    path.delimiter + path.join(wallaby.localProjectDir, 'server', 'node_modules') +
-    path.delimiter + path.join(wallaby.localProjectDir, 'client', 'node_modules')
-
-  console.log("Gandecki process.env.NODE_PATH", process.env.NODE_PATH);
-
   return {
     debug: true,
     testFramework: 'jest',
     files: [
       'client/src/**/*.js',
+      'client/tests/testHelpers/**/*.js',
+      '!client/src/**/*.test.js',
       'server/src/**/*.js',
-      {pattern: '*/src/**/*.spec.js', ignore: true},
-      {pattern: 'client/node_modules/jest/**/*.js', ignore: true},
-      // {pattern: 'client/node_modules/**/*.js', ignore: true},
-      {pattern: 'client/node_modules/jest-runtime/**/*.js', ignore: true},
-      {pattern: 'client/node_modules/jest-cli/**/*.js', ignore: true},
-      {pattern: 'client/node_modules/**/jest-cli/**/*.js', ignore: true},
-      {pattern: 'client/node_modules/react-scripts/**/*.js', ignore: true},
-      // {pattern: 'modules/*(browser|ui)*', ignore: true},
-      // {pattern: 'modules/@(browser|ui)/**/*.js', ignore: true},
-      // {pattern: 'testingMobile/testHelpers/*.js', instrument: true},
-      {pattern: 'client/tests/**/*.js'},
-      {pattern: 'testing/testHelpers/**/*.js'},
-      {pattern: 'mobileClient/tests/testHelpers/**/*.js'},
-      './clientEnzymeTest.json'
+      '!server/src/**/*.spec.js',
+      'cypress/integration/pageObjects/*.js',
+      'testing/testHelpers/**/*.js',
+      'clientEnzymeTest.json'
     ],
     tests: [
-      // 'testing/resolvers.spec.js',
-      // 'testing/test.spec.js',
       'cypress/integration/landingPage.test.js',
     ],
     compilers: {'**/*.js': wallaby.compilers.babel()},
     env: {type: 'node'},
     setup: function (wallaby) {
-      var jestConfig = require('./clientEnzymeTest');
-      jestConfig.globals = { "__DEV__": true };
+      const jestConfig = require('./clientEnzymeTest');
+      jestConfig.moduleDirectories = [
+        'node_modules', '<rootDir>/server/node_modules', '<rootDir>/client/node_modules'
+      ];
       wallaby.testFramework.configure(jestConfig);
     }// --config ./clientEnzymeTest.json
-
   }
 };
