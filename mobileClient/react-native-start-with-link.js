188fb6fe2af8c58776b3110250f030db046aa937
Łukasz Gandecki <lgandecki@css.edu>
2018-01-17 19:49:22 +0400

    make standard happy

--- mobileClient/react-native-start-with-link.js
+++ mobileClient/react-native-start-with-link.js
@@ -4,44 +4,44 @@
  * - react-native start with rn-cli-config
  */
 
-const packageJson = require('./package.json');
-const fs = require('fs');
-const exec = require('child_process').execSync;
-const appJson = require('./app.json');
-const RN_CLI_CONFIG_NAME = `rn-cli-config-with-links.js`;
+const packageJson = require('./package.json')
+const fs = require('fs')
+const exec = require('child_process').execSync
+const appJson = require('./app.json')
+const RN_CLI_CONFIG_NAME = `rn-cli-config-with-links.js`
 
-main();
+main()
 
-function main() {
+function main () {
   const deps = Object.keys(
     Object.assign({}, packageJson.dependencies, packageJson.devDependencies)
-  );
-  const symlinkPathes = getSymlinkPathes(deps);
-  generateRnCliConfig(symlinkPathes, RN_CLI_CONFIG_NAME);
-  generateExpoConfig(RN_CLI_CONFIG_NAME);
-  runBundlerWithConfig(RN_CLI_CONFIG_NAME);
+  )
+  const symlinkPathes = getSymlinkPathes(deps)
+  generateRnCliConfig(symlinkPathes, RN_CLI_CONFIG_NAME)
+  generateExpoConfig(RN_CLI_CONFIG_NAME)
+  runBundlerWithConfig(RN_CLI_CONFIG_NAME)
 }
 
-function getSymlinkPathes(deps) {
-  const depLinks = [];
-  const depPathes = [];
+function getSymlinkPathes (deps) {
+  const depLinks = []
+  const depPathes = []
   deps.forEach(dep => {
-    const stat = fs.lstatSync('node_modules/' + dep);
+    const stat = fs.lstatSync('node_modules/' + dep)
     if (stat.isSymbolicLink()) {
-      depLinks.push(dep);
-      depPathes.push(fs.realpathSync('node_modules/' + dep));
+      depLinks.push(dep)
+      depPathes.push(fs.realpathSync('node_modules/' + dep))
     }
-  });
+  })
 
-  console.log('Starting react native with symlink modules:');
+  console.log('Starting react native with symlink modules:')
   console.log(
     depLinks.map((link, i) => '   ' + link + ' -> ' + depPathes[i]).join('\n')
-  );
+  )
 
-  return depPathes;
+  return depPathes
 }
 
-function generateRnCliConfig(symlinkPathes, configName) {
+function generateRnCliConfig (symlinkPathes, configName) {
   const fileBody = `
 var path = require('path');
 var blacklist;
@@ -77,26 +77,26 @@ var config = {
   }
 };
 module.exports = config;
-  `;
+  `
 
-  fs.writeFileSync(configName, fileBody);
+  fs.writeFileSync(configName, fileBody)
 }
 
-function generateExpoConfig(configName) {
+function generateExpoConfig (configName) {
   const packagerOpts = {
-    projectRoots: "",
-    assetExts: ["ttf"],
+    projectRoots: '',
+    assetExts: ['ttf'],
     config: configName
-  };
-  appJson.expo.packagerOpts = packagerOpts;
+  }
+  appJson.expo.packagerOpts = packagerOpts
   // simply rewrite the expo.packagerOpts, keep the rest identical
-  const fileBody = JSON.stringify(appJson);
-  fs.writeFileSync('app.json', fileBody);
+  const fileBody = JSON.stringify(appJson)
+  fs.writeFileSync('app.json', fileBody)
 }
 
-function runBundlerWithConfig(configName) {
+function runBundlerWithConfig (configName) {
   exec(
     `yarn start`,
     { stdio: [0, 1, 2] }
-  );
+  )
 }
--- mobileClient/rn-cli-config-with-links.js
+++ mobileClient/rn-cli-config-with-links.js
@@ -1,30 +1,29 @@
 
-var path = require('path');
-var blacklist;
+var path = require('path')
+var blacklist
 try {
-  blacklist = require('metro-bundler/src/blacklist');
-} catch(e) {
-  blacklist = require('metro/src/blacklist');
+  blacklist = require('metro-bundler/src/blacklist')
+} catch (e) {
+  blacklist = require('metro/src/blacklist')
 }
 
 var config = {
   extraNodeModules: {
     'react-native': path.resolve(__dirname, 'node_modules/react-native')
   },
-  getBlacklistRE() {
+  getBlacklistRE () {
     return blacklist([
       /[/\\]Users[/\\]lukaszgandecki[/\\]projects[/\\]TheBrain2.0[/\\]shared[/\\]node_modules[/\\]react-native[/\\].*/
-    ]);
+    ])
   },
-  getProjectRoots() {
+  getProjectRoots () {
     return [
       // Keep your project directory.
       path.resolve(__dirname),
 
       // Include your forked package as a new root.
       path.resolve('/Users/lukaszgandecki/projects/TheBrain2.0/shared')
-    ];
+    ]
   }
-};
-module.exports = config;
-  
+}
+module.exports = config
