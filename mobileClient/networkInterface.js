5d4b3095116fff95a3e2603083ec128b93c37fbe
Łukasz Gandecki <lgandecki@css.edu>
2018-01-22 18:55:11 +0400

    e2e mobile test working locally, first attempt to run on circle

--- mobileClient/networkInterface.js
+++ mobileClient/networkInterface.js
@@ -1,5 +1,5 @@
 import { createNetworkInterface } from 'apollo-client'
-const graphqlUri = 'http://192.168.101.124:8080/graphql'
+const graphqlUri = 'http://127.0.0.1:8080/graphql'
 
 const networkInterface = createNetworkInterface({
   uri: graphqlUri,
---
+++ mobileClient/tests/e2e/first.spec.js
@@ -0,0 +1,35 @@
+/* eslint-env jest node */
+/* global jest element by */
+require('babel-polyfill')
+const { reloadApp } = require('detox-expo-helpers')
+const detox = require('detox')
+const config = require('../../package.json').detox
+
+beforeAll(async () => {
+})
+
+afterAll(async () => {
+  await detox.cleanup()
+})
+
+// const timeout = ms => new Promise(res => setTimeout(res, ms))
+
+jest.setTimeout(14000)
+describe('Example', () => {
+  beforeAll(async () => {
+    await detox.init(config)
+    await reloadApp()
+    console.log('finished reloading')
+  })
+
+  it('should have welcome screen', async () => {
+    console.log('welcome screen shown')
+    await element(by.id('skip_intro_button')).tap()
+    await expect(element(by.id('skip_intro_button'))).toBeNotVisible()
+  }, 15000)
+
+  it('should show hello screen after tap', async () => {
+    // await element(by.id('skip_intro_button')).tap();
+    await expect(element(by.id('skip_intro_button'))).toBeNotVisible()
+  }, 15000)
+})
