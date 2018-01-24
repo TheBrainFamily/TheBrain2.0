4897e2a5ee28285ebf7f3b52a0dcfb9daf829e67
Łukasz Gandecki <lgandecki@css.edu>
2018-01-24 19:20:21 +0400

    use expo published app in the e2e tests

--- mobileClient/tests/e2e/first.spec.js
+++ mobileClient/tests/e2e/first.spec.js
@@ -1,5 +1,5 @@
 /* eslint-env jest node */
-/* global jest element by */
+/* global jest element by device waitFor */
 require('babel-polyfill')
 const { reloadApp } = require('detox-expo-helpers')
 const detox = require('detox')
@@ -18,12 +18,24 @@ jest.setTimeout(14000)
 describe('Example', async () => {
   beforeAll(async () => {
     await detox.init(config)
-    await reloadApp()
+    if (process.env.DETOX_EXTERNAL_LINK) {
+      await device.launchApp({
+        newInstance: true,
+        url: process.env.DETOX_EXTERNAL_LINK,
+        sourceApp: 'host.exp.exponent',
+        launchArgs: {EXKernelDisableNuxDefaultsKey: true}
+      })
+    } else {
+      await reloadApp()
+    }
     console.log('finished reloading')
   })
 
   it('should have welcome screen', async () => {
     console.log('welcome screen shown')
+    // waiting for the app the expo app to boot, after that we shouldn't have to use manual waitFors
+    await waitFor(element(by.id('skip_intro_button'))).toBeVisible().withTimeout(10000)
+
     await element(by.id('skip_intro_button')).tap()
     await expect(element(by.id('skip_intro_button'))).toBeNotVisible()
   }, 15000)
