287e6b3788d5f8ebb03e2d400273383ef3a8fe67
Łukasz Gandecki <lgandecki@css.edu>
2018-01-25 20:29:37 +0400

    skip the optional checks for whether the collections are empty

--- testing/common/serverStateHelpers/helpers/reposWithDefaults.js
+++ testing/common/serverStateHelpers/helpers/reposWithDefaults.js
@@ -4,52 +4,47 @@ import { FlashcardsRepository } from '../../../../server/src/api/repositories/Fl
 
 let getCoursesRepoWithDefaults = async function () {
   const coursesRepository = new CoursesRepository()
-  if (!await coursesRepository.coursesCollection.findOne()) {
-    await coursesRepository.coursesCollection.insert({_id: 'testCourseId', name: 'Biology', color: '#62c46c'})
-    await coursesRepository.coursesCollection.insert({_id: 'testCourse2Id', name: 'Chemistry', color: '#662d91'})
-  }
+  await coursesRepository.coursesCollection.insert({_id: 'testCourseId', name: 'Biology', color: '#62c46c'})
+  await coursesRepository.coursesCollection.insert({_id: 'testCourse2Id', name: 'Chemistry', color: '#662d91'})
   return coursesRepository
 }
 
 let getFlashcardsRepoWithDefaults = async function () {
   const flashcardsRepository = new FlashcardsRepository()
-  if (!await flashcardsRepository.flashcardsCollection.findOne()) {
-    await flashcardsRepository.flashcardsCollection.insert({
-      _id: 'fOneId',
-      'question': 'What is the name of this course',
-      'answer': 'Biology'
-    })
-    await flashcardsRepository.flashcardsCollection.insert({
-      _id: 'fTwoId',
-      'question': "How many letters are in the word 'Biology'?",
-      'answer': '7'
-    })
-  }
+  await flashcardsRepository.flashcardsCollection.insert({
+    _id: 'fOneId',
+    'question': 'What is the name of this course',
+    'answer': 'Biology'
+  })
+  await flashcardsRepository.flashcardsCollection.insert({
+    _id: 'fTwoId',
+    'question': 'How many letters are in the word \'Biology\'?',
+    'answer': '7'
+  })
   return flashcardsRepository
 }
 let getLessonsRepoWithDefaults = async function () {
   const lessonsRepository = new LessonsRepository()
-  if (!await lessonsRepository.lessonsCollection.findOne()) {
-    await lessonsRepository.lessonsCollection.insert(
-      {
-        _id: 'lessonId',
-        position: 1,
-        description: 'first lesson',
-        flashcardIds: ['fOneId', 'fTwoId'],
-        youtubeId: 'QnQe0xW_JY4',
-        courseId: 'testCourseId'
-      },
-      {
-        _id: 'lessonId2',
-        position: 2,
-        description: 'second lesson',
-        flashcardIds: ['fSecondLessonOneId', 'fSecondLessonTwoId', 'fSecondLessonThreeId'],
-        youtubeId: 'QnQe0xW_JY4',
-        courseId: 'testCourseId'
-      }
-    )
-  }
+  await lessonsRepository.lessonsCollection.insert(
+    {
+      _id: 'lessonId',
+      position: 1,
+      description: 'first lesson',
+      flashcardIds: ['fOneId', 'fTwoId'],
+      youtubeId: 'QnQe0xW_JY4',
+      courseId: 'testCourseId'
+    })
+
+  await lessonsRepository.lessonsCollection.insert({
+    _id: 'lessonId2',
+    position: 2,
+    description: 'second lesson',
+    flashcardIds: ['fSecondLessonOneId', 'fSecondLessonTwoId', 'fSecondLessonThreeId'],
+    youtubeId: 'QnQe0xW_JY4',
+    courseId: 'testCourseId'
+  })
+
   return lessonsRepository
 }
 
-export {getCoursesRepoWithDefaults, getLessonsRepoWithDefaults, getFlashcardsRepoWithDefaults}
+export { getCoursesRepoWithDefaults, getLessonsRepoWithDefaults, getFlashcardsRepoWithDefaults }
