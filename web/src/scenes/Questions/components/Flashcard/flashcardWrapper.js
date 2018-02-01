4b697689bbc44d3cc8974db794ba24bad01d07d8
Jan Mozgawa <jmozgawa@gmail.com>
2018-02-01 22:46:14 +0100

    move duplicated graphql mutation code do shared getGraphqlFotSetUserIsCasualMutation function

--- web/src/scenes/Questions/components/Flashcard/flashcardWrapper.js
+++ web/src/scenes/Questions/components/Flashcard/flashcardWrapper.js
@@ -1,14 +1,13 @@
 // @flow
 import { connect } from 'react-redux'
 import { compose, graphql } from 'react-apollo'
-import update from 'immutability-helper'
 import { withRouter } from 'react-router'
 
 import userDetailsQuery from 'thebrain-shared/graphql/queries/userDetails'
 import currentUserQuery from 'thebrain-shared/graphql/queries/currentUser'
 import currentItemsQuery from 'thebrain-shared/graphql/queries/itemsWithFlashcard'
-import setUserIsCasualMutation from 'thebrain-shared/graphql/mutations/setUserIsCasual'
 import { getGraphqlForProcessEvaluationMutation } from 'thebrain-shared/graphql/mutations/processEvaluation'
+import { getGraphqlFotSetUserIsCasualMutation } from 'thebrain-shared/graphql/mutations/setUserIsCasual'
 
 import LevelUpWrapper from '../../../../components/LevelUpWrapper'
 
@@ -42,25 +41,5 @@ export const flashcardWrapper = compose(
     }
   }),
   getGraphqlForProcessEvaluationMutation(graphql),
-  graphql(setUserIsCasualMutation, {
-    props: ({ownProps, mutate}) => ({
-      setUserIsCasual: (isCasual) => mutate({
-        variables: {
-          isCasual
-        },
-        updateQueries: {
-          UserDetails: (prev, {mutationResult}) => {
-            return update(prev, {
-              UserDetails: {
-                $set: mutationResult.data.setUserIsCasual
-              }
-            })
-          }
-        },
-        refetchQueries: [{
-          query: currentItemsQuery
-        }]
-      })
-    })
-  })
+  getGraphqlFotSetUserIsCasualMutation(graphql)
 )
