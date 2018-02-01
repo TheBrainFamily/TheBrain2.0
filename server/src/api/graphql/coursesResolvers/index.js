c9d74ae0553d3620d4d38a90ab5d7bb3574f2345
Jan Mozgawa <jmozgawa@gmail.com>
2018-02-02 00:33:24 +0100

    refactor selectCourse and selectCourseSaveToken to share same code

--- server/src/api/graphql/coursesResolvers/index.js
+++ server/src/api/graphql/coursesResolvers/index.js
@@ -10,14 +10,12 @@ export const coursesResolvers = {
     },
     Course (root: ?string, args: { _id: string }, passedContext: Object) {
       const context = {...repositoriesContext, ...passedContext}
-
       return context.Courses.getCourse(args._id)
     }
   },
   Mutation: {
     async selectCourse (root: ?string, args: { courseId: string }, passedContext: Object) {
       const context = {...repositoriesContext, ...passedContext}
-
       let userId = context.user && context.user._id
       if (!userId) {
         const guestUser = await loginWithGuest(root, args, context)
@@ -26,17 +24,10 @@ export const coursesResolvers = {
       return context.UserDetails.selectCourse(userId, args.courseId)
     },
     async selectCourseSaveToken (root: ?string, args: { courseId: string, deviceId: string }, passedContext: Object) {
-      const context = {...repositoriesContext, ...passedContext}
-      let userId = context.user && context.user._id
-      if (!userId) {
-        const guestUser = await loginWithGuest(root, args, context)
-        userId = guestUser._id
-      }
-      return context.UserDetails.selectCourse(userId, args.courseId)
+      return coursesResolvers.Mutation.selectCourse(root, args, passedContext)
     },
     async closeCourse (root: ?string, args: ?Object, passedContext: Object) {
       const context = {...repositoriesContext, ...passedContext}
-
       let userId = context.user && context.user._id
       return context.UserDetails.closeCourse(userId)
     }
